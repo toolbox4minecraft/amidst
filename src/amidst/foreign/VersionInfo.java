568f6b889ea63bfd47a6c854ae3c003a9d07bf16
Skiphs <skiphs.programming@gmail.com>
2013-09-12 07:59:23 -0700

    added 13w37a to official verison list

--- src/amidst/foreign/VersionInfo.java
+++ src/amidst/foreign/VersionInfo.java
@@ -8,6 +8,7 @@ import java.util.List;
  */
 public enum VersionInfo {
 	unknown(null),
+	V13w37a("nsms[Lkl;jf[J[J[J[J[J[[J"),
 	V13w36b("nkmk[Lkd;hw[J[J[J[J[J[[J"),
 	V13w36a("nkmk[Lkd;hx[J[J[J[J[J[[J"),
 	V1_6_2("mulu[Ljr;hm[J[J[J[J[J[[J"),
