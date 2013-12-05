f06b928cf79d6f1f80009ce7c369f0d5c1372154
Skiphs <skiphs.programming@gmail.com>
2013-12-04 23:02:46 -0800

    Updated version text to just "3.5"

--- src/amidst/Amidst.java
+++ src/amidst/Amidst.java
@@ -15,7 +15,7 @@ import amidst.preferences.BiomeColorProfile;
 public class Amidst {
 	public final static int version_major = 3;
 	public final static int version_minor = 5;
-	public final static String versionOffset = " beta 1";
+	public final static String versionOffset = "";
 	public static InstallInformation installInformation = new InstallInformation(true);
 	public static final Gson gson = new Gson();
 	
