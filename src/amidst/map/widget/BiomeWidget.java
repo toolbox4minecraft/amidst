537faefffd98212aa2e6b9b95a922fea688367f6
skiphs <skiphs.programming@gmail.com>
2014-01-10 18:31:20 -0800

    Major improvements to the visuals of the Biome Widget

--- src/amidst/map/widget/BiomeWidget.java
+++ src/amidst/map/widget/BiomeWidget.java
@@ -3,16 +3,23 @@ package amidst.map.widget;
 import java.awt.Color;
 import java.awt.FontMetrics;
 import java.awt.Graphics2D;
+import java.awt.Rectangle;
+import java.awt.Shape;
 import java.util.ArrayList;
 
-import amidst.logging.Log;
-import amidst.map.layers.BiomeLayer;
 import amidst.minecraft.Biome;
 import MoF.MapViewer;
 
 public class BiomeWidget extends PanelWidget {
+	private static Color innerBoxBgColor = new Color(0.3f, 0.3f, 0.3f, 0.3f);
+	private static Color biomeBgColor1 = new Color(0.8f, 0.8f, 0.8f, 0.2f);
+	private static Color biomeBgColor2 = new Color(0.6f, 0.6f, 0.6f, 0.2f);
+	private static Color innerBoxBorderColor = new Color(1.0f, 1.0f, 1.0f, 1.0f);
 	private ArrayList<Biome> biomes = new ArrayList<Biome>();
 	private int maxNameWidth = 0;
+	private Rectangle innerBox = new Rectangle(0, 0, 1, 1);
+	
+	
 	public BiomeWidget(MapViewer mapViewer) {
 		super(mapViewer);
 		
@@ -33,6 +40,41 @@ public class BiomeWidget extends PanelWidget {
 		x = mapViewer.getWidth() - width;
 		super.draw(g2d, time);
 		g2d.setColor(textColor);
+		g2d.setFont(textFont);
+		g2d.drawString("Highlight Biomes", x + 10, y + 20);
+		
+		innerBox.x = x + 10;
+		innerBox.y = y + 30;
+		innerBox.width = width - 20;
+		innerBox.height = height - 40;
+		g2d.setColor(innerBoxBgColor);
+		g2d.fillRect(innerBox.x, innerBox.y, innerBox.width, innerBox.height);
+		g2d.setColor(innerBoxBorderColor);
+		g2d.drawRect(innerBox.x - 1, innerBox.y - 1, innerBox.width + 1, innerBox.height + 1);
+		g2d.setClip(innerBox);
+		
+		for (int i = 0; i < biomes.size(); i++) {
+			Biome biome = biomes.get(i);
+			g2d.setColor(((i % 2) == 1)?biomeBgColor1:biomeBgColor2);
+			g2d.fillRect(innerBox.x, innerBox.y + i * 16,innerBox.width, 16);
+			g2d.setColor(new Color(biome.color));
+			g2d.fillRect(x + 10, innerBox.y + i*16, 20, 16);
+			g2d.setColor(Color.white);
+			g2d.drawString(biome.name, x + 35, innerBox.y + 13 + i*16);
+		}
+		
+		g2d.setClip(null);
+	}
+	
+	@Override
+	public boolean onMouseWheelMoved(int mouseX, int mouseY, int notches) {
+		if ((mouseX > innerBox.x) &&
+			(mouseX < innerBox.x + innerBox.width) &&
+			(mouseY > innerBox.y) &&
+			(mouseY < innerBox.y + innerBox.height)) {
+			
+		}
+		return true;
 	}
 	
 	@Override
