a6b6149523a474dec7a5b432dc0c05d94af05934
Stefan Dollase <stefandollase@gmail.com>
2015-08-29 23:52:04 +0200

    enhanced readability of CCStringMatch

--- src/amidst/bytedata/CCStringMatch.java
+++ src/amidst/bytedata/CCStringMatch.java
@@ -2,13 +2,14 @@ package amidst.bytedata;
 
 import amidst.minecraft.Minecraft;
 
-
 public class CCStringMatch extends ClassChecker {
 	private String checkData;
-	public CCStringMatch(String name, String data) {
+
+	public CCStringMatch(String name, String checkData) {
 		super(name);
-		checkData = data;
+		this.checkData = checkData;
 	}
+
 	@Override
 	public void check(Minecraft m, ByteClass bClass) {
 		if (bClass.searchForString(checkData)) {
@@ -16,5 +17,4 @@ public class CCStringMatch extends ClassChecker {
 			complete();
 		}
 	}
-	
 }
