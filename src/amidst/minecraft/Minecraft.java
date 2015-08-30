a717b0cb54996a36c4f2dac4d95f1ff24f51b370
Stefan Dollase <stefandollase@gmail.com>
2015-08-30 17:08:51 +0200

    added applied eclipse formatting to Minecraft class

--- src/amidst/minecraft/Minecraft.java
+++ src/amidst/minecraft/Minecraft.java
@@ -30,10 +30,15 @@ public class Minecraft {
 	private static final int MAX_CLASSES = 128;
 	private Class<?> mainClass;
 	private URLClassLoader classLoader;
-	private String versionID; 
+	private String versionID;
 	private URL urlToJar;
 	private File jarFile;
-	
+
+	// @formatter:off
+	// This deactivates the automatic formatter of Eclipse.
+	// However, you need to activate this in:
+	// Java -> Code Style -> Formatter -> Edit -> Off/On Tags
+	// see: http://stackoverflow.com/questions/1820908/how-to-turn-off-the-eclipse-code-formatter-for-certain-sections-of-java-code
 	private static ClassChecker[] classChecks = ClassChecker.builder()
 			.matchWildcardBytes("IntCache").data(DeobfuscationData.intCache).end()
 			.matchString("WorldType").data("default_1_1").end()
@@ -80,41 +85,49 @@ public class Minecraft {
 				.end()
 			.end()
 		.construct();
+	// @formatter:on
 
 	private HashMap<String, ByteClass> byteClassMap;
 	private HashMap<String, MinecraftClass> nameMap;
 	private HashMap<String, MinecraftClass> classMap;
 	private Vector<String> byteClassNames;
-	
+
 	public String versionId;
 	public VersionInfo version = VersionInfo.unknown;
-	
-	public Minecraft(File jarFile)  throws MalformedURLException {
+
+	public Minecraft(File jarFile) throws MalformedURLException {
 		this.jarFile = jarFile;
 		byteClassNames = new Vector<String>();
 		byteClassMap = new HashMap<String, ByteClass>(MAX_CLASSES);
 		urlToJar = jarFile.toURI().toURL();
-		
+
 		Log.i("Reading minecraft.jar...");
 		if (!jarFile.exists())
-			Log.crash("Attempted to load jar file at: " + jarFile + " but it does not exist.");
+			Log.crash("Attempted to load jar file at: " + jarFile
+					+ " but it does not exist.");
 		Stack<ByteClass> byteClassStack = new Stack<ByteClass>();
 		try {
 			ZipFile jar = new ZipFile(jarFile);
 			Enumeration<? extends ZipEntry> enu = jar.entries();
 			ByteClassFactory byteClassFactory = ByteClass.factory();
-			
+
 			while (enu.hasMoreElements()) {
 				ZipEntry entry = enu.nextElement();
 				String currentEntry = entry.getName();
 				String[] nameSplit = currentEntry.split("\\.");
-				if (!entry.isDirectory() && (nameSplit.length == 2) && (nameSplit[0].indexOf('/') == -1) && nameSplit[1].equals("class")) {
-					BufferedInputStream is = new BufferedInputStream(jar.getInputStream(entry));
-					if (is.available() < 8000) { // TODO: Double check that this filter won't mess anything up.
+				if (!entry.isDirectory() && (nameSplit.length == 2)
+						&& (nameSplit[0].indexOf('/') == -1)
+						&& nameSplit[1].equals("class")) {
+					BufferedInputStream is = new BufferedInputStream(
+							jar.getInputStream(entry));
+					if (is.available() < 8000) { // TODO: Double check that this
+													// filter won't mess
+													// anything up.
 						byte[] classData = new byte[is.available()];
 						is.read(classData);
 						is.close();
-						byteClassStack.push(byteClassFactory.create(nameSplit[0], classData));
+						byteClassStack.push(byteClassFactory.create(
+								nameSplit[0], classData));
 					}
 				}
 			}
@@ -124,7 +137,7 @@ public class Minecraft {
 			e.printStackTrace();
 			Log.crash(e, "Error extracting jar data.");
 		}
-		
+
 		Log.i("Searching for classes...");
 		int checksRemaining = classChecks.length;
 		Object[] byteClasses = byteClassStack.toArray();
@@ -133,14 +146,17 @@ public class Minecraft {
 			for (int q = 0; q < classChecks.length; q++) {
 				for (int i = 0; i < byteClasses.length; i++) {
 					if (!found[q]) {
-						classChecks[q].check(this, (ByteClass)byteClasses[i]);
+						classChecks[q].check(this, (ByteClass) byteClasses[i]);
 						if (classChecks[q].isComplete()) {
-							Log.debug("Found: " + byteClasses[i] + " as " + classChecks[q].getName() + " | " + classChecks[q].getClass().getSimpleName());
+							Log.debug("Found: " + byteClasses[i] + " as "
+									+ classChecks[q].getName() + " | "
+									+ classChecks[q].getClass().getSimpleName());
 							found[q] = true;
 							checksRemaining--;
 						}
 						// TODO: What is this line, and why is it commented
-						//byteClassMap.put(classChecks[q].getName(), classFiles[i].getName().split("\\.")[0]);
+						// byteClassMap.put(classChecks[q].getName(),
+						// classFiles[i].getName().split("\\.")[0]);
 					}
 				}
 				if (!found[q]) {
@@ -150,25 +166,29 @@ public class Minecraft {
 						checksRemaining--;
 					}
 				}
-				
 
 			}
 		}
 		Log.i("Class search complete.");
-		
+
 		Log.i("Generating version ID...");
 		use();
 		try {
 			use();
-			if (classLoader.findResource("net/minecraft/client/Minecraft.class") != null)
-				mainClass = classLoader.loadClass("net.minecraft.client.Minecraft");
-			else if (classLoader.findResource("net/minecraft/server/MinecraftServer.class") != null)
-				mainClass = classLoader.loadClass("net.minecraft.server.MinecraftServer");
+			if (classLoader
+					.findResource("net/minecraft/client/Minecraft.class") != null)
+				mainClass = classLoader
+						.loadClass("net.minecraft.client.Minecraft");
+			else if (classLoader
+					.findResource("net/minecraft/server/MinecraftServer.class") != null)
+				mainClass = classLoader
+						.loadClass("net.minecraft.server.MinecraftServer");
 			else
 				throw new RuntimeException();
 		} catch (Exception e) {
 			e.printStackTrace(); // TODO: Make this exception far less broad.
-			Log.crash(e, "Attempted to load non-minecraft jar, or unable to locate starting point.");
+			Log.crash(e,
+					"Attempted to load non-minecraft jar, or unable to locate starting point.");
 		}
 		String typeDump = "";
 		Field fields[] = null;
@@ -176,9 +196,11 @@ public class Minecraft {
 			fields = mainClass.getDeclaredFields();
 		} catch (NoClassDefFoundError e) {
 			e.printStackTrace();
-			Log.crash(e, "Unable to find critical external class while loading.\nPlease ensure you have the correct Minecraft libraries installed.");
+			Log.crash(
+					e,
+					"Unable to find critical external class while loading.\nPlease ensure you have the correct Minecraft libraries installed.");
 		}
-		
+
 		for (int i = 0; i < fields.length; i++) {
 			String typeString = fields[i].getType().toString();
 			if (typeString.startsWith("class ") && !typeString.contains("."))
@@ -192,54 +214,73 @@ public class Minecraft {
 			}
 		}
 
-		Log.i("Identified Minecraft [" + version.name() + "] with versionID of " + versionId);
+		Log.i("Identified Minecraft [" + version.name()
+				+ "] with versionID of " + versionId);
 		Log.i("Loading classes...");
 		nameMap = new HashMap<String, MinecraftClass>();
 		classMap = new HashMap<String, MinecraftClass>();
 
 		for (String name : byteClassNames) {
 			ByteClass byteClass = byteClassMap.get(name);
-			MinecraftClass minecraftClass = new MinecraftClass(name, byteClass.getClassName());
+			MinecraftClass minecraftClass = new MinecraftClass(name,
+					byteClass.getClassName());
 			minecraftClass.load(this);
 			nameMap.put(minecraftClass.getName(), minecraftClass);
 			classMap.put(minecraftClass.getClassName(), minecraftClass);
 		}
-		
+
 		for (MinecraftClass minecraftClass : nameMap.values()) {
 			ByteClass byteClass = byteClassMap.get(minecraftClass.getName());
 			for (String[] property : byteClass.getProperties())
-				minecraftClass.addProperty(new MinecraftProperty(minecraftClass, property[1], property[0]));
+				minecraftClass.addProperty(new MinecraftProperty(
+						minecraftClass, property[1], property[0]));
 			for (String[] method : byteClass.getMethods()) {
 				String methodString = obfuscateStringClasses(method[0]);
-				methodString = methodString.replaceAll(",INVALID", "").replaceAll("INVALID,","").replaceAll("INVALID", "");
+				methodString = methodString.replaceAll(",INVALID", "")
+						.replaceAll("INVALID,", "").replaceAll("INVALID", "");
 				String methodDeobfName = method[1];
-				String methodObfName = methodString.substring(0, methodString.indexOf('('));
-				String parameterString = methodString.substring(methodString.indexOf('(') + 1, methodString.indexOf(')'));
-				
+				String methodObfName = methodString.substring(0,
+						methodString.indexOf('('));
+				String parameterString = methodString.substring(
+						methodString.indexOf('(') + 1,
+						methodString.indexOf(')'));
+
 				if (parameterString.equals("")) {
-					minecraftClass.addMethod(new MinecraftMethod(minecraftClass, methodDeobfName, methodObfName));
+					minecraftClass.addMethod(new MinecraftMethod(
+							minecraftClass, methodDeobfName, methodObfName));
 				} else {
 					String[] parameterClasses = parameterString.split(",");
-					minecraftClass.addMethod(new MinecraftMethod(minecraftClass, methodDeobfName, methodObfName, parameterClasses));
+					minecraftClass.addMethod(new MinecraftMethod(
+							minecraftClass, methodDeobfName, methodObfName,
+							parameterClasses));
 				}
 			}
 			for (String[] constructor : byteClass.getConstructors()) {
-				String methodString = obfuscateStringClasses(constructor[0]).replaceAll(",INVALID", "").replaceAll("INVALID,","").replaceAll("INVALID", "");
+				String methodString = obfuscateStringClasses(constructor[0])
+						.replaceAll(",INVALID", "").replaceAll("INVALID,", "")
+						.replaceAll("INVALID", "");
 				String methodDeobfName = constructor[1];
-				String methodObfName = methodString.substring(0, methodString.indexOf('('));
-				String parameterString = methodString.substring(methodString.indexOf('(') + 1, methodString.indexOf(')'));
-				
+				String methodObfName = methodString.substring(0,
+						methodString.indexOf('('));
+				String parameterString = methodString.substring(
+						methodString.indexOf('(') + 1,
+						methodString.indexOf(')'));
+
 				if (parameterString.equals("")) {
-					minecraftClass.addMethod(new MinecraftMethod(minecraftClass, methodDeobfName, methodObfName));
+					minecraftClass.addMethod(new MinecraftMethod(
+							minecraftClass, methodDeobfName, methodObfName));
 				} else {
 					String[] parameterClasses = parameterString.split(",");
-					minecraftClass.addMethod(new MinecraftMethod(minecraftClass, methodDeobfName, methodObfName, parameterClasses));
+					minecraftClass.addMethod(new MinecraftMethod(
+							minecraftClass, methodDeobfName, methodObfName,
+							parameterClasses));
 				}
 			}
 		}
 		Log.i("Classes loaded.");
 		Log.i("Minecraft load complete.");
 	}
+
 	private String obfuscateStringClasses(String inString) {
 		inString = inString.replaceAll(" ", "");
 		Pattern cPattern = Pattern.compile("@[A-Za-z]+");
@@ -249,7 +290,8 @@ public class Minecraft {
 			String match = inString.substring(cMatcher.start(), cMatcher.end());
 			ByteClass byteClass = getByteClass(match.substring(1));
 			if (byteClass != null) {
-				tempOutput = tempOutput.replaceAll(match, byteClass.getClassName());
+				tempOutput = tempOutput.replaceAll(match,
+						byteClass.getClassName());
 			} else {
 				tempOutput = tempOutput.replaceAll(match, "INVALID");
 			}
@@ -257,12 +299,11 @@ public class Minecraft {
 		}
 		return tempOutput;
 	}
-	
-	
+
 	public URL getPath() {
 		return urlToJar;
 	}
-	
+
 	private Stack<URL> getLibraries(File jsonFile) {
 		Log.i("Loading libraries.");
 		Stack<URL> libraries = new Stack<URL>();
@@ -270,10 +311,11 @@ public class Minecraft {
 		try {
 			profile = Util.readObject(jsonFile, JarProfile.class);
 		} catch (IOException e) {
-			Log.w("Invalid jar profile loaded. Library loading will be skipped. (Path: " + jsonFile + ")");
+			Log.w("Invalid jar profile loaded. Library loading will be skipped. (Path: "
+					+ jsonFile + ")");
 			return libraries;
 		}
-		
+
 		for (JarLibrary library : profile.getLibraries()) {
 			File libraryFile = library.getFile();
 			if (libraryFile != null) {
@@ -281,18 +323,19 @@ public class Minecraft {
 					libraries.add(libraryFile.toURI().toURL());
 					Log.i("Found library: " + libraryFile);
 				} catch (MalformedURLException e) {
-					Log.w("Unable to convert library file to URL with path: " + libraryFile);
+					Log.w("Unable to convert library file to URL with path: "
+							+ libraryFile);
 					e.printStackTrace();
 				}
 			} else {
 				Log.i("Skipping library: " + library.getName());
 			}
 		}
-		
+
 		return libraries;
 	}
-	
-	/* 
+
+	/*-
 	 * This was the old search-and-add-all libraries method. This may still be useful
 	 * if the user doesn't have a json file, or mojang changes the format.
 	 * 
@@ -312,12 +355,12 @@ public class Minecraft {
 		}
 		return urls;
 	}
-	*/
-	
+	 */
+
 	public void use() {
-		File librariesJson = Options.instance.minecraftJson == null ?
-				new File(jarFile.getPath().replace(".jar", ".json"))
-			  : new File(Options.instance.minecraftJson);
+		File librariesJson = Options.instance.minecraftJson == null ? new File(
+				jarFile.getPath().replace(".jar", ".json")) : new File(
+				Options.instance.minecraftJson);
 		if (librariesJson.exists()) {
 			Stack<URL> libraries = getLibraries(librariesJson);
 			URL[] libraryArray = new URL[libraries.size() + 1];
@@ -325,21 +368,25 @@ public class Minecraft {
 			libraryArray[libraries.size()] = urlToJar;
 			classLoader = new URLClassLoader(libraryArray);
 		} else {
-			Log.i("Unable to find Minecraft library JSON at: " + librariesJson + ". Skipping.");
+			Log.i("Unable to find Minecraft library JSON at: " + librariesJson
+					+ ". Skipping.");
 			classLoader = new URLClassLoader(new URL[] { urlToJar });
 		}
 		Thread.currentThread().setContextClassLoader(classLoader);
 	}
-	
+
 	public String getVersionID() {
 		return versionID;
 	}
+
 	public MinecraftClass getClassByName(String name) {
 		return nameMap.get(name);
 	}
+
 	public URLClassLoader getClassLoader() {
 		return classLoader;
 	}
+
 	public Class<?> loadClass(String name) {
 		try {
 			return classLoader.loadClass(name);
@@ -349,24 +396,25 @@ public class Minecraft {
 		}
 		return null;
 	}
+
 	public MinecraftClass getClassByType(String name) {
 		return classMap.get(name);
-		
-		
-		
+
 	}
+
 	public void registerClass(String publicName, ByteClass bClass) {
-		if (byteClassMap.get(publicName)==null) {
+		if (byteClassMap.get(publicName) == null) {
 			byteClassMap.put(publicName, bClass);
 			byteClassNames.add(publicName);
 		}
 	}
+
 	public ByteClass getByteClass(String name) {
 		return byteClassMap.get(name);
 	}
-	
+
 	public IMinecraftInterface createInterface() {
 		return new LocalMinecraftInterface(this);
 	}
-	
+
 }
