c1e3e96f9f0c78cc946a14ed99f29a6de9a515a1
Stefan Dollase <stefandollase@gmail.com>
2015-11-11 21:43:11 +0100

    applied eclipse formatting to Fragment and FragmentManager

--- src/amidst/map/Fragment.java
+++ src/amidst/map/Fragment.java
@@ -11,74 +11,80 @@ import amidst.logging.Log;
 import amidst.minecraft.MinecraftUtil;
 
 public class Fragment {
-	public static final int SIZE = 512, SIZE_SHIFT = 9, MAX_OBJECTS_PER_FRAGMENT = 32, BIOME_SIZE = SIZE >> 2;
+	public static final int SIZE = 512, SIZE_SHIFT = 9,
+			MAX_OBJECTS_PER_FRAGMENT = 32, BIOME_SIZE = SIZE >> 2;
 	private static AffineTransform drawMatrix = new AffineTransform();
 	public int blockX, blockY;
-	
+
 	public short[] biomeData = new short[BIOME_SIZE * BIOME_SIZE];
-	
+
 	private ImageLayer[] imageLayers;
 	private LiveLayer[] liveLayers;
 	private IconLayer[] iconLayers;
-	
+
 	private Object loadLock = new Object();
-	
+
 	private BufferedImage[] images;
 	public MapObject[] objects;
 	public int objectsLength = 0;
-	
+
 	private float alpha = 0.0f;
-	
+
 	public boolean isActive = false;
 	public boolean isLoaded = false;
-	
+
 	public Fragment nextFragment = null, prevFragment = null;
 	public boolean hasNext = false;
-	
+
 	public boolean endOfLine = false;
-	
-	private static int[] dataCache = new int[SIZE*SIZE];
-	
+
+	private static int[] dataCache = new int[SIZE * SIZE];
+
 	public Fragment(ImageLayer... layers) {
 		this(layers, null, null);
 	}
-	public Fragment(ImageLayer[] imageLayers, LiveLayer[] liveLayers, IconLayer[] iconLayers) {
+
+	public Fragment(ImageLayer[] imageLayers, LiveLayer[] liveLayers,
+			IconLayer[] iconLayers) {
 		this.imageLayers = imageLayers;
 		this.liveLayers = liveLayers;
 		images = new BufferedImage[imageLayers.length];
 		for (int i = 0; i < imageLayers.length; i++)
-			images[imageLayers[i].getLayerId()] = new BufferedImage(imageLayers[i].size, imageLayers[i].size, BufferedImage.TYPE_INT_ARGB);
+			images[imageLayers[i].getLayerId()] = new BufferedImage(
+					imageLayers[i].size, imageLayers[i].size,
+					BufferedImage.TYPE_INT_ARGB);
 		this.iconLayers = iconLayers;
 		objects = new MapObject[MAX_OBJECTS_PER_FRAGMENT];
 	}
-	
+
 	public void load() {
 		synchronized (loadLock) {
 			if (isLoaded)
 				Log.w("This should never happen!");
-			int[] data = MinecraftUtil.getBiomeData(blockX >> 2, blockY >> 2, BIOME_SIZE, BIOME_SIZE, true);
+			int[] data = MinecraftUtil.getBiomeData(blockX >> 2, blockY >> 2,
+					BIOME_SIZE, BIOME_SIZE, true);
 			for (int i = 0; i < BIOME_SIZE * BIOME_SIZE; i++)
-				biomeData[i] = (short)data[i];
+				biomeData[i] = (short) data[i];
 			for (int i = 0; i < imageLayers.length; i++)
 				imageLayers[i].load(this);
 			for (int i = 0; i < iconLayers.length; i++)
 				iconLayers[i].generateMapObjects(this);
-			alpha = Options.instance.mapFading.get()?0.0f:1.0f;
+			alpha = Options.instance.mapFading.get() ? 0.0f : 1.0f;
 			isLoaded = true;
 		}
 	}
-	
+
 	public void recycle() {
-		isActive = false;		
+		isActive = false;
 		isLoaded = false;
 	}
-	
+
 	public void clearData() {
 		for (IconLayer layer : iconLayers)
 			layer.clearMapObjects(this);
 		isLoaded = false;
 	}
-	
+
 	public void clear() {
 		for (IconLayer layer : iconLayers)
 			layer.clearMapObjects(this);
@@ -86,41 +92,47 @@ public class Fragment {
 		endOfLine = false;
 		isActive = true;
 	}
-	
+
 	public void drawLiveLayers(float time, Graphics2D g, AffineTransform mat) {
 		for (int i = 0; i < liveLayers.length; i++) {
 			if (liveLayers[i].isVisible()) {
 				liveLayers[i].drawLive(this, g, mat);
 			}
 		}
-		
+
 	}
+
 	public void drawImageLayers(float time, Graphics2D g, AffineTransform mat) {
 		if (!isLoaded)
 			return;
-		
-		alpha = Math.min(1.0f, time*3.0f + alpha);
+
+		alpha = Math.min(1.0f, time * 3.0f + alpha);
 		for (int i = 0; i < images.length; i++) {
 			if (imageLayers[i].isVisible()) {
-				g.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER, alpha * imageLayers[i].getAlpha()));
+				g.setComposite(AlphaComposite.getInstance(
+						AlphaComposite.SRC_OVER,
+						alpha * imageLayers[i].getAlpha()));
 
-				
 				// TOOD: FIX THIS
 				g.setTransform(imageLayers[i].getScaledMatrix(mat));
 				if (g.getTransform().getScaleX() < 1.0f)
-					g.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BILINEAR);
+					g.setRenderingHint(RenderingHints.KEY_INTERPOLATION,
+							RenderingHints.VALUE_INTERPOLATION_BILINEAR);
 				else
-					g.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_NEAREST_NEIGHBOR);
+					g.setRenderingHint(RenderingHints.KEY_INTERPOLATION,
+							RenderingHints.VALUE_INTERPOLATION_NEAREST_NEIGHBOR);
 				g.drawImage(images[i], 0, 0, null);
 			}
 		}
-		g.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER, 1.0f));
+		g.setComposite(AlphaComposite
+				.getInstance(AlphaComposite.SRC_OVER, 1.0f));
 	}
-	
+
 	public void drawObjects(Graphics2D g, AffineTransform inMatrix) {
 		if (alpha != 1.0f)
-			g.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER, alpha));
-		
+			g.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER,
+					alpha));
+
 		for (int i = 0; i < objectsLength; i++) {
 			if (objects[i].parentLayer.isVisible()) {
 				drawMatrix.setTransform(inMatrix);
@@ -128,19 +140,18 @@ public class Fragment {
 				double invZoom = 1.0 / objects[i].parentLayer.getMapZoom();
 				drawMatrix.scale(invZoom, invZoom);
 				g.setTransform(drawMatrix);
-				
-				g.drawImage(objects[i].getImage(), 
-							-(objects[i].getWidth() >> 1),
-							-(objects[i].getHeight() >> 1),
-							objects[i].getWidth(),
-							objects[i].getHeight(),
-							null);
+
+				g.drawImage(objects[i].getImage(),
+						-(objects[i].getWidth() >> 1),
+						-(objects[i].getHeight() >> 1), objects[i].getWidth(),
+						objects[i].getHeight(), null);
 			}
 		}
 		if (alpha != 1.0f)
-			g.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER, 1.0f));
+			g.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER,
+					1.0f));
 	}
-	
+
 	public void addObject(MapObject object) {
 		object.rx = object.x + this.blockX;
 		object.ry = object.y + this.blockY;
@@ -153,49 +164,53 @@ public class Fragment {
 		objects[objectsLength] = object;
 		objectsLength++;
 	}
-	
+
 	public void setImageData(int layerId, int[] data) {
-		images[layerId].setRGB(0, 0, imageLayers[layerId].size, imageLayers[layerId].size, data, 0, imageLayers[layerId].size);
+		images[layerId].setRGB(0, 0, imageLayers[layerId].size,
+				imageLayers[layerId].size, data, 0, imageLayers[layerId].size);
 	}
-	
-	
+
 	public int getBlockX() {
 		return blockX;
 	}
+
 	public int getBlockY() {
 		return blockY;
 	}
+
 	public int getChunkX() {
 		return blockX >> 4;
 	}
+
 	public int getChunkY() {
 		return blockY >> 4;
 	}
+
 	public int getFragmentX() {
 		return blockX >> SIZE_SHIFT;
 	}
+
 	public int getFragmentY() {
 		return blockY >> SIZE_SHIFT;
 	}
-	
+
 	public void setNext(Fragment frag) {
 		nextFragment = frag;
 		frag.prevFragment = this;
 		hasNext = true;
 	}
-	
+
 	public void remove() {
 		if (hasNext)
 			prevFragment.setNext(nextFragment);
 		else
 			prevFragment.hasNext = false;
 	}
-	
-	
+
 	public static int[] getIntArray() {
 		return dataCache;
 	}
-	
+
 	public void removeObject(MapObjectPlayer player) {
 		for (int i = 0; i < objectsLength; i++) {
 			if (objects[i] == player) {
@@ -204,20 +219,23 @@ public class Fragment {
 			}
 		}
 	}
+
 	public BufferedImage getBufferedImage(int layer) {
 		return images[layer];
 	}
+
 	public void reset() {
 		objectsLength = 0;
 		isActive = false;
 		isLoaded = false;
-		
+
 		nextFragment = null;
 		prevFragment = null;
 		hasNext = false;
-		
+
 		endOfLine = false;
 	}
+
 	public void repaint() {
 		synchronized (loadLock) {
 			if (isLoaded)
@@ -225,7 +243,7 @@ public class Fragment {
 					imageLayers[i].load(this);
 		}
 	}
-	
+
 	public void repaintImageLayer(int id) {
 		synchronized (loadLock) {
 			if (isLoaded)
--- src/amidst/map/FragmentManager.java
+++ src/amidst/map/FragmentManager.java
@@ -8,31 +8,32 @@ import amidst.logging.Log;
 
 public class FragmentManager implements Runnable {
 	private int cacheSize = 1024;
-	
+
 	private Thread currentThread;
 	private boolean running = true;
-	
+
 	private Fragment[] fragmentCache;
 	private ConcurrentLinkedQueue<Fragment> fragmentQueue;
 	private ConcurrentLinkedQueue<Fragment> requestQueue;
 	private ConcurrentLinkedQueue<Fragment> recycleQueue;
 	private int sleepTick = 0;
-	
+
 	private Object queueLock = new Object();
-	
+
 	private Stack<ImageLayer> layerList;
-	
+
 	private ImageLayer[] imageLayers;
 	private IconLayer[] iconLayers;
 	private LiveLayer[] liveLayers;
-	
-	public FragmentManager(ImageLayer[] imageLayers, LiveLayer[] liveLayers, IconLayer[] iconLayers) {
+
+	public FragmentManager(ImageLayer[] imageLayers, LiveLayer[] liveLayers,
+			IconLayer[] iconLayers) {
 		fragmentQueue = new ConcurrentLinkedQueue<Fragment>();
 		requestQueue = new ConcurrentLinkedQueue<Fragment>();
 		recycleQueue = new ConcurrentLinkedQueue<Fragment>();
 		layerList = new Stack<ImageLayer>();
 		Collections.addAll(layerList, imageLayers);
-		
+
 		fragmentCache = new Fragment[cacheSize];
 		for (int i = 0; i < imageLayers.length; i++)
 			imageLayers[i].setLayerId(i);
@@ -44,6 +45,7 @@ public class FragmentManager implements Runnable {
 		this.iconLayers = iconLayers;
 		this.liveLayers = liveLayers;
 	}
+
 	public void updateLayers(float time) {
 		for (ImageLayer layer : imageLayers)
 			layer.update(time);
@@ -52,7 +54,7 @@ public class FragmentManager implements Runnable {
 		for (IconLayer layer : iconLayers)
 			layer.update(time);
 	}
-	
+
 	public void reset() {
 		running = false;
 		try {
@@ -60,7 +62,7 @@ public class FragmentManager implements Runnable {
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
-		
+
 		recycleQueue.clear();
 		requestQueue.clear();
 		fragmentQueue.clear();
@@ -69,7 +71,7 @@ public class FragmentManager implements Runnable {
 			fragmentQueue.offer(fragmentCache[i]);
 		}
 	}
-	
+
 	private void increaseFragmentCache() {
 		Fragment[] newFragments = new Fragment[cacheSize << 1];
 		for (int i = 0; i < cacheSize; i++) {
@@ -81,23 +83,25 @@ public class FragmentManager implements Runnable {
 			fragmentQueue.offer(newFragments[i]);
 		}
 		fragmentCache = newFragments;
-		Log.i("FragmentManager cache size increased from " + cacheSize + " to " + (cacheSize << 1));
+		Log.i("FragmentManager cache size increased from " + cacheSize + " to "
+				+ (cacheSize << 1));
 		cacheSize <<= 1;
 		System.gc();
 	}
-	
+
 	public void repaintFragment(Fragment frag) {
 		synchronized (queueLock) {
 			frag.repaint();
 		}
 	}
+
 	public Fragment requestFragment(int x, int y) {
 		if (!running)
 			return null;
 		Fragment frag = null;
 		while ((frag = fragmentQueue.poll()) == null)
 			increaseFragmentCache();
-		
+
 		frag.clear();
 		frag.blockX = x;
 		frag.blockY = y;
@@ -105,17 +109,17 @@ public class FragmentManager implements Runnable {
 		requestQueue.offer(frag);
 		return frag;
 	}
-	
+
 	public void returnFragment(Fragment frag) {
 		recycleQueue.offer(frag);
 	}
-	
+
 	@Override
 	public void run() {
 		currentThread.setPriority(Thread.MIN_PRIORITY);
 
 		while (running) {
-			if(!requestQueue.isEmpty() || !recycleQueue.isEmpty()) {
+			if (!requestQueue.isEmpty() || !recycleQueue.isEmpty()) {
 				if (!requestQueue.isEmpty()) {
 					synchronized (queueLock) {
 						Fragment frag = requestQueue.poll();
@@ -126,12 +130,13 @@ public class FragmentManager implements Runnable {
 								sleepTick = 0;
 								try {
 									Thread.sleep(1L);
-								} catch (InterruptedException ignored) {}
+								} catch (InterruptedException ignored) {
+								}
 							}
 						}
 					}
 				}
-				
+
 				while (!recycleQueue.isEmpty()) {
 					synchronized (queueLock) {
 						Fragment frag = recycleQueue.poll();
@@ -143,28 +148,29 @@ public class FragmentManager implements Runnable {
 				sleepTick = 0;
 				try {
 					Thread.sleep(2L);
-				} catch (InterruptedException ignored) {}
+				} catch (InterruptedException ignored) {
+				}
 			}
 		}
-		
+
 	}
-	
+
 	public void setMap(Map map) {
 		for (ImageLayer layer : imageLayers) {
 			layer.setMap(map);
 			layer.reload();
 		}
-		
+
 		for (LiveLayer layer : liveLayers) {
 			layer.setMap(map);
 			layer.reload();
 		}
-		
+
 		for (IconLayer layer : iconLayers) {
 			layer.setMap(map);
 			layer.reload();
 		}
-		
+
 		currentThread = new Thread(this);
 
 		running = true;
@@ -174,15 +180,19 @@ public class FragmentManager implements Runnable {
 	public int getCacheSize() {
 		return cacheSize;
 	}
+
 	public int getFreeFragmentQueueSize() {
 		return fragmentQueue.size();
 	}
+
 	public int getRecycleQueueSize() {
 		return recycleQueue.size();
 	}
+
 	public int getRequestQueueSize() {
 		return requestQueue.size();
 	}
+
 	public void repaintFragmentLayer(Fragment frag, int id) {
 		synchronized (queueLock) {
 			frag.repaintImageLayer(id);
