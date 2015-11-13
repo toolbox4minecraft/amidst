66fb2e2d98190cec432cb0efaec19d4c65f8a290
Stefan Dollase <stefandollase@gmail.com>
2015-11-13 15:44:10 +0100

    applied eclipse formatting to the class VersionSelectWindow

--- src/amidst/gui/version/VersionSelectWindow.java
+++ src/amidst/gui/version/VersionSelectWindow.java
@@ -22,64 +22,69 @@ import amidst.version.VersionFactory;
 public class VersionSelectWindow extends JFrame {
 	private static VersionSelectWindow instance;
 	private VersionFactory versionFactory = new VersionFactory();
-	
+
 	public VersionSelectWindow() {
 		super("Profile Selector");
 		instance = this;
 		setIconImage(Amidst.icon);
 		Container contentPane = getContentPane();
 		contentPane.setLayout(new MigLayout());
-		
+
 		LatestVersionList.get().load(true);
-		
-		if (!Util.minecraftDirectory.exists() || !Util.minecraftDirectory.isDirectory()) {
-			Log.crash("Unable to find Minecraft directory at: " + Util.minecraftDirectory);
+
+		if (!Util.minecraftDirectory.exists()
+				|| !Util.minecraftDirectory.isDirectory()) {
+			Log.crash("Unable to find Minecraft directory at: "
+					+ Util.minecraftDirectory);
 			return;
 		}
-		
-		final JLabel titleLabel = new JLabel("Please select a Minecraft version:", SwingConstants.CENTER);
+
+		final JLabel titleLabel = new JLabel(
+				"Please select a Minecraft version:", SwingConstants.CENTER);
 		titleLabel.setFont(new Font("arial", Font.BOLD, 16));
-		
+
 		add(titleLabel, "h 20!,w :400:, growx, pushx, wrap");
 
 		final VersionSelectPanel versionSelector = new VersionSelectPanel();
-		
+
 		(new Thread(new Runnable() {
 			@Override
 			public void run() {
 				versionFactory.scanForProfiles();
 				MinecraftProfile[] localVersions = versionFactory.getProfiles();
 				String selectedProfile = Options.instance.lastProfile.get();
-				
+
 				if (localVersions == null) {
 					versionSelector.setEmptyMessage("Empty");
 					return;
 				}
 				for (int i = 0; i < localVersions.length; i++)
-					versionSelector.addVersion(new LocalVersionComponent(localVersions[i]));
+					versionSelector.addVersion(new LocalVersionComponent(
+							localVersions[i]));
 				versionSelector.addVersion(new RemoteVersionComponent());
-				
+
 				if (selectedProfile != null)
 					versionSelector.select(selectedProfile);
-				
+
 				pack();
 				try {
 					Thread.sleep(100);
-				} catch (InterruptedException ignored) { }
+				} catch (InterruptedException ignored) {
+				}
 				pack();
 			}
 		})).start();
-		
+
 		versionSelector.setEmptyMessage("Scanning...");
-		
+
 		JScrollPane scrollPane = new JScrollPane(versionSelector);
 		add(scrollPane, "grow, push, h 80::");
 		pack();
 		setLocation(200, 200);
 		setVisible(true);
-		
+
 		addKeyListener(versionSelector);
-		
+
 		addWindowListener(new WindowAdapter() {
 			@Override
 			public void windowClosing(WindowEvent e) {
@@ -88,7 +93,7 @@ public class VersionSelectWindow extends JFrame {
 			}
 		});
 	}
-	
+
 	public static VersionSelectWindow get() {
 		return instance;
 	}
