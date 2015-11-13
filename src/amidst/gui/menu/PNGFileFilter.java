717d45ab774378cc1ec436bc99d54a5554cd15e8
Stefan Dollase <stefandollase@gmail.com>
2015-11-13 06:23:42 +0100

    refactored the class PNGFileFilter

--- src/amidst/gui/menu/PNGFileFilter.java
+++ src/amidst/gui/menu/PNGFileFilter.java
@@ -1,17 +1,28 @@
 package amidst.gui.menu;
 
-import javax.swing.filechooser.FileFilter;
 import java.io.File;
+
+import javax.swing.filechooser.FileFilter;
 
 public class PNGFileFilter extends FileFilter {
 	@Override
 	public boolean accept(File file) {
-		if (file.isDirectory())
+		if (file.isDirectory()) {
 			return true;
-		String[] st = file.getName().split("/.");
-		return st[st.length - 1].equalsIgnoreCase("png");
+		} else {
+			return getFileExtension(file.getName()).equalsIgnoreCase("png");
+		}
 	}
-	
+
+	private String getFileExtension(String fileName) {
+		int i = fileName.lastIndexOf('.');
+		if (i > 0) {
+			return fileName.substring(i + 1);
+		} else {
+			return "";
+		}
+	}
+
 	@Override
 	public String getDescription() {
 		return "Portable Network Graphic (*.PNG)";
