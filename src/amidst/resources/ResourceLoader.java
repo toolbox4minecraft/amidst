8bf29ef9d34eade0e1cbbd1629270adb8b2487eb
Stefan Dollase <stefandollase@gmail.com>
2015-11-14 05:33:01 +0100

    refactored the class ResourceLoader

--- src/amidst/resources/ResourceLoader.java
+++ src/amidst/resources/ResourceLoader.java
@@ -1,29 +1,44 @@
 package amidst.resources;
 
-import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.URISyntaxException;
 import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+import javax.imageio.ImageIO;
 
 public class ResourceLoader {
-	private ResourceLoader() {}
-	
+	private ResourceLoader() {
+	}
+
 	public static URL getResourceURL(String name) {
-		// This is also a valid way to load resources, although I'm not sure which is better.
-		//return ClassLoader.getSystemClassLoader().getResource("amidst/resources/" + name);
 		return ResourceLoader.class.getResource(name);
 	}
-	
+
 	public static InputStream getResourceStream(String name) {
 		return ResourceLoader.class.getResourceAsStream(name);
 	}
-	
+
 	public static BufferedImage getImage(String name) {
 		try {
 			return ImageIO.read(getResourceURL(name));
-		} catch (IOException e) { //Don't forget to run the tests :)
+		} catch (IOException e) {
 			throw new RuntimeException(e);
 		}
+	}
+
+	public static String getResourceAsString(String name) throws IOException,
+			URISyntaxException {
+		return new String(Files.readAllBytes(getResourceAsPath(name)),
+				StandardCharsets.UTF_8);
+	}
+
+	public static Path getResourceAsPath(String name) throws URISyntaxException {
+		return new File(getResourceURL(name).toURI()).toPath();
 	}
 }
