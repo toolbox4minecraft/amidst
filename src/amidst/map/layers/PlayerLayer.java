fff0d64aa37b5033aa97457dfed0ed8477bfacf8
Stefan Dollase <stefandollase@gmail.com>
2015-11-15 03:44:29 +0100

    applied eclipse formatting to the class PlayerLayer

--- src/amidst/map/layers/PlayerLayer.java
+++ src/amidst/map/layers/PlayerLayer.java
@@ -13,47 +13,51 @@ public class PlayerLayer extends IconLayer {
 	public PlayerMover saveLoader;
 	public static SkinManager skinManager = new SkinManager();
 	public boolean isEnabled;
+
 	static {
 		skinManager.start();
 	}
+
 	public PlayerLayer() {
-		
+
 	}
-	
+
 	@Override
 	public boolean isVisible() {
-		return Options.instance.showPlayers.get();		
+		return Options.instance.showPlayers.get();
 	}
-	
+
 	@Override
 	public void generateMapObjects(Fragment frag) {
-		if (!isEnabled) return;
-		List<MapObjectPlayer> players =  saveLoader.getPlayers();
+		if (!isEnabled)
+			return;
+		List<MapObjectPlayer> players = saveLoader.getPlayers();
 		for (MapObjectPlayer player : players) {
-			if ((player.globalX >= frag.getBlockX()) &&
-				(player.globalX < frag.getBlockX() + Fragment.SIZE) &&
-				(player.globalY >= frag.getBlockY()) &&
-				(player.globalY < frag.getBlockY() + Fragment.SIZE)) {
+			if ((player.globalX >= frag.getBlockX())
+					&& (player.globalX < frag.getBlockX() + Fragment.SIZE)
+					&& (player.globalY >= frag.getBlockY())
+					&& (player.globalY < frag.getBlockY() + Fragment.SIZE)) {
 				player.parentLayer = this;
 				player.parentFragment = frag;
 				frag.addObject(player);
 			}
 		}
 	}
-	
+
 	@Override
 	public void clearMapObjects(Fragment frag) {
 		for (int i = 0; i < frag.getObjectsLength(); i++) {
 			if (frag.getObjects()[i] instanceof MapObjectPlayer)
-				((MapObjectPlayer)frag.getObjects()[i]).parentFragment = null;
-			
+				((MapObjectPlayer) frag.getObjects()[i]).parentFragment = null;
+
 		}
 		super.clearMapObjects(frag);
-		
+
 	}
+
 	public void setPlayers(PlayerMover save) {
 		saveLoader = save;
-		
+
 		for (MapObjectPlayer player : saveLoader.getPlayers())
 			skinManager.addPlayer(player);
 	}
