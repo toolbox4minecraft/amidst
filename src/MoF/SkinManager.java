0dc8d57f305c98a91deb5ef99b28d29d1d645dd2
Stefan Dollase <stefandollase@gmail.com>
2015-11-15 06:45:22 +0100

    refactored the class SkinManager

--- src/MoF/SkinManager.java
+++ src/MoF/SkinManager.java
@@ -6,62 +6,91 @@ import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.util.Stack;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.TimeUnit;
 
 import javax.imageio.ImageIO;
 
+import amidst.logging.Log;
 import amidst.map.MapObjectPlayer;
 
 public class SkinManager extends Thread {
-	private Stack<MapObjectPlayer> players;
-	public boolean active;
+	private BlockingQueue<MapObjectPlayer> playerQueue = new LinkedBlockingQueue<MapObjectPlayer>();
+	private boolean active = true;
 
-	public SkinManager() {
-		players = new Stack<MapObjectPlayer>();
-		active = true;
-	}
-
-	public void addPlayer(MapObjectPlayer p) {
-		players.push(p);
+	public void addPlayer(MapObjectPlayer player) {
+		try {
+			playerQueue.put(player);
+		} catch (InterruptedException e) {
+			Log.w("Cannot enqueue player");
+		}
 	}
 
 	public void run() {
-		while (this.active) {
-			try {
-				if (players.isEmpty()) {
-					Thread.sleep(50L);
-				} else {
-					MapObjectPlayer p = players.pop();
-					try {
-						URL url = new URL(
-								"http://s3.amazonaws.com/MinecraftSkins/"
-										+ p.getName() + ".png");
-						BufferedImage img = ImageIO.read(url);
-						BufferedImage pimg = new BufferedImage(20, 20,
-								BufferedImage.TYPE_INT_ARGB);
-						Graphics2D g2d = pimg.createGraphics();
-						g2d.setColor(Color.black);
-						g2d.fillRect(0, 0, 20, 20);
-						g2d.drawImage(img, 2, 2, 18, 18, 8, 8, 16, 16, null);
-						g2d.dispose();
-						img.flush();
-						p.setMarker(pimg);
-						Thread.sleep(20L);
-					} catch (MalformedURLException e2) {
-					} catch (IOException e) {
-					}
+		while (active) {
+			MapObjectPlayer player = getNextPlayer();
+			if (player != null) {
+				try {
+					loadSkin(player);
+				} catch (MalformedURLException e) {
+					error(player, e);
+				} catch (IOException e) {
+					error(player, e);
 				}
-			} catch (InterruptedException e) {
-
 			}
-		}
-		if (!this.active) {
-			dispose();
 		}
 	}
 
+	private void error(MapObjectPlayer player, Exception e) {
+		Log.w("Cannot load skin for player " + player.getName());
+		e.printStackTrace();
+	}
+
+	private MapObjectPlayer getNextPlayer() {
+		try {
+			return playerQueue.poll(1, TimeUnit.SECONDS);
+		} catch (InterruptedException e) {
+			return null;
+		}
+	}
+
+	private void loadSkin(MapObjectPlayer player) throws MalformedURLException,
+			IOException {
+		player.setMarker(createMarker(player));
+	}
+
+	private BufferedImage createMarker(MapObjectPlayer player)
+			throws MalformedURLException, IOException {
+		BufferedImage marker = new BufferedImage(20, 20,
+				BufferedImage.TYPE_INT_ARGB);
+		drawSkinToMarker(marker, getSkin(player));
+		return marker;
+	}
+
+	private void drawSkinToMarker(BufferedImage pimg, BufferedImage skin) {
+		Graphics2D g2d = pimg.createGraphics();
+		g2d.setColor(Color.black);
+		g2d.fillRect(0, 0, 20, 20);
+		g2d.drawImage(skin, 2, 2, 18, 18, 8, 8, 16, 16, null);
+		g2d.dispose();
+		skin.flush();
+	}
+
+	private BufferedImage getSkin(MapObjectPlayer player)
+			throws MalformedURLException, IOException {
+		return ImageIO.read(getSkinURL(player));
+	}
+
+	private URL getSkinURL(MapObjectPlayer player) throws MalformedURLException {
+		return new URL("http://s3.amazonaws.com/MinecraftSkins/"
+				+ player.getName() + ".png");
+	}
+
+	// TODO: call me!
 	public void dispose() {
-		players.clear();
-		players = null;
+		active = false;
+		playerQueue.clear();
+		playerQueue = null;
 	}
 }
