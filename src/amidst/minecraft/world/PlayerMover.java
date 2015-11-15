917e49727df5e2178cc845b1c37671b126e383a8
Stefan Dollase <stefandollase@gmail.com>
2015-11-15 02:35:54 +0100

    switched the class PlayerMover to use the class Player directly

--- src/amidst/minecraft/world/PlayerMover.java
+++ src/amidst/minecraft/world/PlayerMover.java
@@ -14,6 +14,8 @@ import org.jnbt.DoubleTag;
 import org.jnbt.ListTag;
 import org.jnbt.Tag;
 
+import amidst.minecraft.world.World.Player;
+
 import com.esotericsoftware.minlog.Log;
 
 public class PlayerMover {
@@ -25,10 +27,10 @@ public class PlayerMover {
 		this.isMultiPlayerMap = isMultiPlayerMap;
 	}
 
-	public void movePlayer(String playerName, int x, int y) {
-		File file = getPlayerFile(playerName);
+	public void movePlayer(Player player) {
+		File file = getPlayerFile(player);
 		if (createBackup(file)) {
-			movePlayer(file, x, y);
+			doMovePlayer(player, file);
 		} else {
 			// TODO: gui feedback
 			Log.warn("Creation of backup file failed. Skipping player movement.");
@@ -66,125 +68,125 @@ public class PlayerMover {
 		}
 	}
 
-	private void movePlayer(File file, int x, int y) {
+	private void doMovePlayer(Player player, File file) {
 		if (isMultiPlayerMap) {
 			try {
-				movePlayerOnMultiPlayerMap(file, x, y);
+				movePlayerOnMultiPlayerMap(player, file);
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
 		} else {
 			try {
-				movePlayerOnSinglePlayerMap(file, x, y);
+				movePlayerOnSinglePlayerMap(player, file);
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
 		}
 	}
 
-	private File getPlayerFile(String playerName) {
+	private File getPlayerFile(Player player) {
 		if (isMultiPlayerMap) {
-			return getMultiPlayerPlayerFile(playerName);
+			return getMultiPlayerPlayerFile(player.getPlayerName());
 		} else {
 			return file;
 		}
 	}
 
-	private File getMultiPlayerPlayerFile(String name) {
-		return new File(file.getParent() + "/players/" + name + ".dat");
+	private File getMultiPlayerPlayerFile(String playerName) {
+		return new File(file.getParent() + "/players/" + playerName + ".dat");
 	}
 
-	private void movePlayerOnMultiPlayerMap(File file, int x, int y)
+	private void movePlayerOnMultiPlayerMap(Player player, File file)
 			throws IOException, FileNotFoundException {
 		CompoundTag dataTag = NBTUtils.readTagFromFile(file);
 		CompoundTag modifiedDataTag = modifyPositionInDataTagMultiPlayer(
-				dataTag, x, y);
+				dataTag, player);
 		NBTUtils.writeTagToFile(file, modifiedDataTag);
 	}
 
-	private void movePlayerOnSinglePlayerMap(File file, int x, int y)
+	private void movePlayerOnSinglePlayerMap(Player player, File file)
 			throws IOException, FileNotFoundException {
 		CompoundTag baseTag = NBTUtils.readTagFromFile(file);
 		CompoundTag modifiedBaseTag = modifyPositionInBaseTagSinglePlayer(
-				baseTag, x, y);
+				baseTag, player);
 		NBTUtils.writeTagToFile(file, modifiedBaseTag);
 	}
 
 	private CompoundTag modifyPositionInBaseTagSinglePlayer(
-			CompoundTag baseTag, int x, int y) {
+			CompoundTag baseTag, Player player) {
 		Map<String, Tag> baseMap = baseTag.getValue();
 		Map<String, Tag> modifiedBaseMap = modifyPositionInBaseMapSinglePlayer(
-				baseMap, x, y);
+				baseMap, player);
 		return new CompoundTag(NBTTagKeys.TAG_KEY_BASE, modifiedBaseMap);
 	}
 
 	private Map<String, Tag> modifyPositionInBaseMapSinglePlayer(
-			Map<String, Tag> baseMap, int x, int y) {
+			Map<String, Tag> baseMap, Player player) {
 		Map<String, Tag> result = new HashMap<String, Tag>();
 		CompoundTag dataTag = (CompoundTag) baseMap
 				.get(NBTTagKeys.TAG_KEY_DATA);
 		CompoundTag modifiedDataTag = modifyPositionInDataTagSinglePlayer(
-				dataTag, x, y);
+				dataTag, player);
 		result.put(NBTTagKeys.TAG_KEY_DATA, modifiedDataTag);
 		return result;
 	}
 
 	private CompoundTag modifyPositionInDataTagSinglePlayer(
-			CompoundTag dataTag, int x, int y) {
+			CompoundTag dataTag, Player player) {
 		Map<String, Tag> dataMap = dataTag.getValue();
 		Map<String, Tag> modifiedDataMap = modifyPositionInDataMapSinglePlayer(
-				dataMap, x, y);
+				dataMap, player);
 		return new CompoundTag(NBTTagKeys.TAG_KEY_DATA, modifiedDataMap);
 	}
 
 	private Map<String, Tag> modifyPositionInDataMapSinglePlayer(
-			Map<String, Tag> dataMap, int x, int y) {
+			Map<String, Tag> dataMap, Player player) {
 		Map<String, Tag> result = new HashMap<String, Tag>(dataMap);
 		CompoundTag playerTag = (CompoundTag) dataMap
 				.get(NBTTagKeys.TAG_KEY_PLAYER);
 		CompoundTag modifiedPlayerTag = modifyPositionInPlayerTagSinglePlayer(
-				playerTag, x, y);
+				playerTag, player);
 		result.put(NBTTagKeys.TAG_KEY_PLAYER, modifiedPlayerTag);
 		return result;
 	}
 
 	private CompoundTag modifyPositionInPlayerTagSinglePlayer(
-			CompoundTag playerTag, int x, int y) {
+			CompoundTag playerTag, Player player) {
 		Map<String, Tag> playerMap = playerTag.getValue();
 		Map<String, Tag> modifiedPlayerMap = modifyPositionInPlayerMap(
-				playerMap, x, y);
+				playerMap, player);
 		return new CompoundTag(NBTTagKeys.TAG_KEY_PLAYER, modifiedPlayerMap);
 	}
 
 	private CompoundTag modifyPositionInDataTagMultiPlayer(CompoundTag dataTag,
-			int x, int y) {
+			Player player) {
 		Map<String, Tag> playerMap = dataTag.getValue();
 		Map<String, Tag> modifiedPlayerMap = modifyPositionInPlayerMap(
-				playerMap, x, y);
+				playerMap, player);
 		return new CompoundTag(NBTTagKeys.TAG_KEY_DATA, modifiedPlayerMap);
 	}
 
 	private Map<String, Tag> modifyPositionInPlayerMap(
-			Map<String, Tag> playerMap, int x, int y) {
+			Map<String, Tag> playerMap, Player player) {
 		Map<String, Tag> result = new HashMap<String, Tag>(playerMap);
 		ListTag posTag = (ListTag) playerMap.get(NBTTagKeys.TAG_KEY_POS);
-		ListTag modifiedPosTag = modifyPositionInPosTag(posTag, x, y);
+		ListTag modifiedPosTag = modifyPositionInPosTag(posTag, player);
 		result.put(NBTTagKeys.TAG_KEY_POS, modifiedPosTag);
 		return result;
 	}
 
-	private ListTag modifyPositionInPosTag(ListTag posTag, int x, int y) {
+	private ListTag modifyPositionInPosTag(ListTag posTag, Player player) {
 		List<Tag> posList = posTag.getValue();
-		List<Tag> modifiedPosList = modifyPositionInPosList(posList, x, y);
+		List<Tag> modifiedPosList = modifyPositionInPosList(posList, player);
 		return new ListTag(NBTTagKeys.TAG_KEY_POS, DoubleTag.class,
 				modifiedPosList);
 	}
 
-	private List<Tag> modifyPositionInPosList(List<Tag> posList, int x, int y) {
+	private List<Tag> modifyPositionInPosList(List<Tag> posList, Player player) {
 		List<Tag> result = new ArrayList<Tag>(posList);
-		result.set(0, new DoubleTag("x", x));
+		result.set(0, new DoubleTag("x", player.getX()));
 		result.set(1, new DoubleTag("y", 120));
-		result.set(2, new DoubleTag("z", y));
+		result.set(2, new DoubleTag("z", player.getZ()));
 		return result;
 	}
 }
