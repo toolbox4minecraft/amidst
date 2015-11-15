c14d9540fe37b100526f3f68847b497662475929
Stefan Dollase <stefandollase@gmail.com>
2015-11-15 22:39:15 +0100

    applied eclipse formatting to the classes BiomeLayer and SlimeLayer

--- src/amidst/map/layers/BiomeLayer.java
+++ src/amidst/map/layers/BiomeLayer.java
@@ -2,10 +2,8 @@ package amidst.map.layers;
 
 import amidst.Options;
 import amidst.Util;
-import amidst.logging.Log;
 import amidst.map.Fragment;
 import amidst.map.ImageLayer;
-import amidst.map.MapViewer;
 import amidst.minecraft.Biome;
 
 public class BiomeLayer extends ImageLayer {
@@ -14,70 +12,78 @@ public class BiomeLayer extends ImageLayer {
 
 	protected boolean[] selectedBiomes = new boolean[Biome.biomes.length];
 	private boolean inHighlightMode = false;
-	
+
 	public BiomeLayer() {
 		super(size);
 		instance = this;
 		deselectAllBiomes();
 	}
-	
+
 	public void selectAllBiomes() {
 		setSelectedAllBiomes(true);
 	}
+
 	public void deselectAllBiomes() {
 		setSelectedAllBiomes(false);
 	}
-	
+
 	public void selectBiome(int id) {
 		setSelected(id, true);
 	}
-	public void deselectBiome(int id) { 
+
+	public void deselectBiome(int id) {
 		setSelected(id, false);
 	}
 
 	public void setHighlightMode(boolean enabled) {
 		inHighlightMode = enabled;
 	}
-	
+
 	public void toggleBiomeSelect(int id) {
 		setSelected(id, !selectedBiomes[id]);
 	}
+
 	public void setSelected(int id, boolean value) {
 		selectedBiomes[id] = value;
 	}
-	
+
 	public void setSelectedAllBiomes(boolean value) {
 		for (int i = 0; i < selectedBiomes.length; i++)
 			selectedBiomes[i] = value;
 	}
-	
+
 	@Override
 	public void drawToCache(Fragment fragment) {
 		int[] dataCache = Fragment.getImageRGBDataCache();
 		if (inHighlightMode) {
-			for (int i = 0; i < size*size; i++)
+			for (int i = 0; i < size * size; i++)
 				if (!selectedBiomes[fragment.getBiomeData()[i]])
-					dataCache[i] = Util.deselectColor(Biome.biomes[fragment.getBiomeData()[i]].color);
+					dataCache[i] = Util.deselectColor(Biome.biomes[fragment
+							.getBiomeData()[i]].color);
 				else
 					dataCache[i] = Biome.biomes[fragment.getBiomeData()[i]].color;
 		} else {
-			for (int i = 0; i < size*size; i++)
+			for (int i = 0; i < size * size; i++)
 				dataCache[i] = Biome.biomes[fragment.getBiomeData()[i]].color;
 		}
 
-		
 		fragment.setImageRGB(getLayerId(), dataCache);
 	}
-	
+
 	public static int getBiomeForFragment(Fragment frag, int blockX, int blockY) {
-		return frag.getBiomeData()[(blockY >> 2) * Fragment.BIOME_SIZE + (blockX >> 2)];
+		return frag.getBiomeData()[(blockY >> 2) * Fragment.BIOME_SIZE
+				+ (blockX >> 2)];
 	}
-	
-	public static String getBiomeNameForFragment(Fragment frag, int blockX, int blockY) {
+
+	public static String getBiomeNameForFragment(Fragment frag, int blockX,
+			int blockY) {
 		return Biome.biomes[getBiomeForFragment(frag, blockX, blockY)].name;
 	}
-	public static String getBiomeAliasForFragment(Fragment frag, int blockX, int blockY) {
-		return Options.instance.biomeColorProfile.getAliasForId(getBiomeForFragment(frag, blockX, blockY));
+
+	public static String getBiomeAliasForFragment(Fragment frag, int blockX,
+			int blockY) {
+		return Options.instance.biomeColorProfile
+				.getAliasForId(getBiomeForFragment(frag, blockX, blockY));
 	}
 
 	public boolean isBiomeSelected(int id) {
--- src/amidst/map/layers/SlimeLayer.java
+++ src/amidst/map/layers/SlimeLayer.java
@@ -9,15 +9,16 @@ import amidst.map.ImageLayer;
 public class SlimeLayer extends ImageLayer {
 	private static int size = Fragment.SIZE >> 4;
 	private Random random = new Random();
+
 	public SlimeLayer() {
 		super(size);
 	}
-	
+
 	@Override
 	public boolean isVisible() {
 		return Options.instance.showSlimeChunks.get();
 	}
-	
+
 	@Override
 	public void drawToCache(Fragment fragment) {
 		int[] dataCache = Fragment.getImageRGBDataCache();
@@ -25,17 +26,16 @@ public class SlimeLayer extends ImageLayer {
 			for (int x = 0; x < size; x++) {
 				int xPosition = fragment.getChunkX() + x;
 				int yPosition = fragment.getChunkY() + y;
-				random.setSeed(Options.instance.seed +
-					xPosition * xPosition * 0x4c1906 + 
-					xPosition * 0x5ac0db + 
-					yPosition * yPosition * 0x4307a7L + 
-					yPosition * 0x5f24f ^ 0x3ad8025f);
-				
-				dataCache[y * size + x] = (random.nextInt(10) == 0) ? 0xA0FF00FF : 0x00000000;
+				random.setSeed(Options.instance.seed + xPosition * xPosition
+						* 0x4c1906 + xPosition * 0x5ac0db + yPosition
+						* yPosition * 0x4307a7L + yPosition * 0x5f24f
+						^ 0x3ad8025f);
+
+				dataCache[y * size + x] = (random.nextInt(10) == 0) ? 0xA0FF00FF
+						: 0x00000000;
 			}
 		}
-		
+
 		fragment.setImageRGB(getLayerId(), dataCache);
 	}
-	
 }
