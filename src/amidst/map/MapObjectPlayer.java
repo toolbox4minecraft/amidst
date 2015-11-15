27461b7b659a3ed0078426bb783976984867321f
Stefan Dollase <stefandollase@gmail.com>
2015-11-15 03:20:06 +0100

    applied eclipse formatting to the class MapObjectPlayer

--- src/amidst/map/MapObjectPlayer.java
+++ src/amidst/map/MapObjectPlayer.java
@@ -8,47 +8,50 @@ public class MapObjectPlayer extends MapObject {
 	private BufferedImage marker;
 	public int globalX, globalY;
 	public Fragment parentFragment = null;
-	
+
 	public MapObjectPlayer(String name, int x, int y) {
-		super(MapMarkers.PLAYER,
-				((x < 0)?Fragment.SIZE:0) + x % Fragment.SIZE,
-				((y < 0)?Fragment.SIZE:0) + y % Fragment.SIZE);
+		super(MapMarkers.PLAYER, ((x < 0) ? Fragment.SIZE : 0) + x
+				% Fragment.SIZE, ((y < 0) ? Fragment.SIZE : 0) + y
+				% Fragment.SIZE);
 		globalX = x;
 		globalY = y;
 		marker = type.image;
 		needSave = false;
 		this.name = name;
 	}
-	
-	
+
 	@Override
 	public int getWidth() {
-		return (int)(marker.getWidth()*localScale);
+		return (int) (marker.getWidth() * localScale);
 	}
+
 	@Override
 	public int getHeight() {
-		return (int)(marker.getHeight()*localScale);
+		return (int) (marker.getHeight() * localScale);
 	}
+
 	public void setPosition(int x, int y) {
 		this.globalX = x;
 		this.globalY = y;
-		this.x = ((x < 0)?Fragment.SIZE:0) + x % Fragment.SIZE;
-		this.y = ((y < 0)?Fragment.SIZE:0) + y % Fragment.SIZE;
+		this.x = ((x < 0) ? Fragment.SIZE : 0) + x % Fragment.SIZE;
+		this.y = ((y < 0) ? Fragment.SIZE : 0) + y % Fragment.SIZE;
 		needSave = true;
 	}
-	
+
 	@Override
 	public BufferedImage getImage() {
 		return marker;
 	}
-	
+
 	public void setMarker(BufferedImage img) {
 		this.marker = img;
 	}
+
 	@Override
 	public String getName() {
 		return name;
 	}
+
 	@Override
 	public String toString() {
 		return "Player \"" + name + "\" at (" + x + ", " + y + ")";
