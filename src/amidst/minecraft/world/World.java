0e0f0ad624850216af402dea4e2545178eed7a7e
Stefan Dollase <stefandollase@gmail.com>
2015-11-21 05:16:51 +0100

    added all finders to the class World

--- src/amidst/minecraft/world/World.java
+++ src/amidst/minecraft/world/World.java
@@ -1,18 +1,24 @@
 package amidst.minecraft.world;
 
+import amidst.minecraft.world.finder.CachedFinder;
+import amidst.minecraft.world.finder.Finder;
 import amidst.minecraft.world.finder.FindingConsumer;
 import amidst.minecraft.world.finder.NetherFortressFinder;
 import amidst.minecraft.world.finder.OceanMonumentFinder;
+import amidst.minecraft.world.finder.PlayerFinder;
+import amidst.minecraft.world.finder.SpawnFinder;
+import amidst.minecraft.world.finder.StrongholdFinder;
 import amidst.minecraft.world.finder.TempleFinder;
 import amidst.minecraft.world.finder.VillageFinder;
 
 public abstract class World {
-	private OceanMonumentFinder oceanMonumentFinder = new OceanMonumentFinder(
-			this);
-	private TempleFinder templeFinder = new TempleFinder(this);
-	private VillageFinder villageFinder = new VillageFinder(this);
-	private NetherFortressFinder netherFortressFinder = new NetherFortressFinder(
-			this);
+	private final Finder oceanMonumentFinder = new OceanMonumentFinder(this);
+	private final Finder templeFinder = new TempleFinder(this);
+	private final Finder villageFinder = new VillageFinder(this);
+	private final Finder netherFortressFinder = new NetherFortressFinder(this);
+	private final CachedFinder playerFinder = new PlayerFinder(this);
+	private final CachedFinder spawnFinder = new SpawnFinder(this);
+	private final CachedFinder strongholdFinder = new StrongholdFinder(this);
 
 	public boolean isFileWorld() {
 		return this instanceof FileWorld;
@@ -44,5 +50,22 @@ public abstract class World {
 	public void getNetherFortresses(CoordinatesInWorld corner,
 			FindingConsumer consumer) {
 		netherFortressFinder.find(corner, consumer);
+	}
+
+	public void getPlayers(CoordinatesInWorld corner, FindingConsumer consumer) {
+		playerFinder.find(corner, consumer);
+	}
+
+	public void getSpawn(CoordinatesInWorld corner, FindingConsumer consumer) {
+		spawnFinder.find(corner, consumer);
+	}
+
+	public void getStrongholds(CoordinatesInWorld corner,
+			FindingConsumer consumer) {
+		strongholdFinder.find(corner, consumer);
+	}
+
+	public void reloadPlayers() {
+		playerFinder.resetCache();
 	}
 }
