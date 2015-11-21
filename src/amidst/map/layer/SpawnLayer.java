9d5a124af8fcd43ed9c06605c08054154e660b9a
Stefan Dollase <stefandollase@gmail.com>
2015-11-21 06:01:47 +0100

    switched SpawnLayer and StrongholdLayer to use producers from world

--- src/amidst/map/layer/SpawnLayer.java
+++ src/amidst/map/layer/SpawnLayer.java
@@ -1,34 +1,9 @@
 package amidst.map.layer;
 
-import java.awt.Point;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Random;
-
 import amidst.Options;
-import amidst.logging.Log;
 import amidst.map.Fragment;
-import amidst.map.MapMarkers;
-import amidst.map.object.MapObject;
-import amidst.minecraft.Biome;
-import amidst.minecraft.MinecraftUtil;
-import amidst.minecraft.world.CoordinatesInWorld;
 
 public class SpawnLayer extends IconLayer {
-	// @formatter:off
-	private static final List<Biome> VALID_BIOMES = Arrays.asList(
-			Biome.forest,
-			Biome.plains,
-			Biome.taiga,
-			Biome.taigaHills,
-			Biome.forestHills,
-			Biome.jungle,
-			Biome.jungleHills
-	);
-	// @formatter:on
-
-	private MapObject spawn;
-
 	@Override
 	public boolean isVisible() {
 		return Options.instance.showSpawn.get();
@@ -36,30 +11,10 @@ public class SpawnLayer extends IconLayer {
 
 	@Override
 	public void generateMapObjects(Fragment fragment) {
-		if (spawn != null && fragment.isInBounds(spawn)) {
-			fragment.addObject(spawn);
-		}
-	}
-
-	@Override
-	public void reload() {
-		initSpawnObject();
-	}
-
-	private void initSpawnObject() {
-		Point spawnCenter = getSpawnCenterInWorldCoordinates();
-		if (spawnCenter != null) {
-			spawn = MapObject.from(
-					CoordinatesInWorld.from(spawnCenter.x, spawnCenter.y),
-					MapMarkers.SPAWN, Options.instance.showSpawn);
-		} else {
-			Log.debug("Unable to find spawn biome.");
-			spawn = null;
-		}
-	}
-
-	private Point getSpawnCenterInWorldCoordinates() {
-		Random random = new Random(Options.instance.world.getSeed());
-		return MinecraftUtil.findValidLocation(0, 0, 256, VALID_BIOMES, random);
+		Options.instance.world
+				.getNetherFortresses(
+						fragment.getCorner(),
+						createWorldObjectConsumer(fragment,
+								Options.instance.showSpawn));
 	}
 }
--- src/amidst/map/layer/StrongholdLayer.java
+++ src/amidst/map/layer/StrongholdLayer.java
@@ -1,71 +1,9 @@
 package amidst.map.layer;
 
-import java.awt.Point;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Random;
-
 import amidst.Options;
 import amidst.map.Fragment;
-import amidst.map.MapMarkers;
-import amidst.map.object.MapObject;
-import amidst.minecraft.Biome;
-import amidst.minecraft.MinecraftUtil;
-import amidst.minecraft.world.CoordinatesInWorld;
-import amidst.version.VersionInfo;
 
 public class StrongholdLayer extends IconLayer {
-	// @formatter:off
-	private static final List<Biome> VALID_BIOMES_DEFAULT = Arrays.asList(
-			Biome.desert,
-			Biome.forest,
-			Biome.extremeHills,
-			Biome.swampland
-	);
-	
-	private static final List<Biome> VALID_BIOMES_1_0 = Arrays.asList(
-			Biome.desert,
-			Biome.forest,
-			Biome.extremeHills,
-			Biome.swampland,
-			Biome.taiga,
-			Biome.icePlains,
-			Biome.iceMountains
-	);
-	
-	private static final List<Biome> VALID_BIOMES_1_1 = Arrays.asList(
-			Biome.desert,
-			Biome.forest,
-			Biome.extremeHills,
-			Biome.swampland,
-			Biome.taiga,
-			Biome.icePlains,
-			Biome.iceMountains,
-			Biome.desertHills,
-			Biome.forestHills,
-			Biome.extremeHillsEdge
-	);
-	
-	private static final List<Biome> VALID_BIOMES_12w03a = Arrays.asList(
-			Biome.desert,
-			Biome.forest,
-			Biome.extremeHills,
-			Biome.swampland,
-			Biome.taiga,
-			Biome.icePlains,
-			Biome.iceMountains,
-			Biome.desertHills,
-			Biome.forestHills,
-			Biome.extremeHillsEdge,
-			Biome.jungle,
-			Biome.jungleHills
-	);
-	// @formatter:on
-
-	private MapObject[] strongholds = new MapObject[3];
-	private Random random = new Random();
-
 	@Override
 	public boolean isVisible() {
 		return Options.instance.showStrongholds.get();
@@ -73,106 +11,9 @@ public class StrongholdLayer extends IconLayer {
 
 	@Override
 	public void generateMapObjects(Fragment fragment) {
-		int size = Fragment.SIZE >> 4;
-		for (int x = 0; x < size; x++) {
-			for (int y = 0; y < size; y++) {
-				generateAt(fragment, x, y);
-			}
-		}
-	}
-
-	private void generateAt(Fragment fragment, int x, int y) {
-		for (int i = 0; i < 3; i++) {
-			if (fragment.isInBounds(strongholds[i])) {
-				fragment.addObject(strongholds[i]);
-			}
-		}
-	}
-
-	@Override
-	public void reload() {
-		findStrongholds();
-	}
-
-	private void findStrongholds() {
-		List<Biome> validBiomes = getValidBiomes();
-		updateSeed();
-		double angle = initAngle();
-		for (int i = 0; i < 3; i++) {
-			double distance = nextDistance();
-			int x = getX(angle, distance) << 4;
-			int y = getY(angle, distance) << 4;
-			Point strongholdLocation = findStronghold(validBiomes, x, y);
-			if (strongholdLocation != null) {
-				strongholds[i] = createMapObject(strongholdLocation.x,
-						strongholdLocation.y);
-			} else {
-				strongholds[i] = createMapObject(x, y);
-			}
-			angle = updateAngle(angle);
-		}
-	}
-
-	private MapObject createMapObject(int x, int y) {
-		return MapObject.from(CoordinatesInWorld.from(x, y),
-				MapMarkers.STRONGHOLD, Options.instance.showStrongholds);
-	}
-
-	private Point findStronghold(List<Biome> validBiomes, int x, int y) {
-		return MinecraftUtil.findValidLocation(x + 8, y + 8, 112, validBiomes,
-				random);
-	}
-
-	private int getY(double angle, double distance) {
-		return (int) Math.round(Math.sin(angle) * distance);
-	}
-
-	private int getX(double angle, double distance) {
-		return (int) Math.round(Math.cos(angle) * distance);
-	}
-
-	private double nextDistance() {
-		return (1.25D + random.nextDouble()) * 32.0D;
-	}
-
-	private double initAngle() {
-		return random.nextDouble() * 3.141592653589793D * 2.0D;
-	}
-
-	private double updateAngle(double angle) {
-		return angle + 6.283185307179586D / 3.0D;
-	}
-
-	private void updateSeed() {
-		random.setSeed(Options.instance.world.getSeed());
-	}
-
-	private List<Biome> getValidBiomes() {
-		if (MinecraftUtil.getVersion().isAtLeast(VersionInfo.V13w36a)) {
-			return getValidBiomesV13w36a();
-		} else if (MinecraftUtil.getVersion().isAtLeast(VersionInfo.V12w03a)) {
-			return VALID_BIOMES_12w03a;
-		} else if (MinecraftUtil.getVersion() == VersionInfo.V1_1) {
-			return VALID_BIOMES_1_1;
-		} else if (MinecraftUtil.getVersion() == VersionInfo.V1_9pre6
-				|| MinecraftUtil.getVersion() == VersionInfo.V1_0) {
-			return VALID_BIOMES_1_0;
-		} else {
-			return VALID_BIOMES_DEFAULT;
-		}
-	}
-
-	private List<Biome> getValidBiomesV13w36a() {
-		List<Biome> result = new ArrayList<Biome>();
-		for (Biome biome : Biome.biomes) {
-			if (isValidBiomeV13w36a(biome)) {
-				result.add(biome);
-			}
-		}
-		return result;
-	}
-
-	private boolean isValidBiomeV13w36a(Biome biome) {
-		return biome != null && biome.type.value1 > 0;
+		Options.instance.world.getNetherFortresses(
+				fragment.getCorner(),
+				createWorldObjectConsumer(fragment,
+						Options.instance.showStrongholds));
 	}
 }
