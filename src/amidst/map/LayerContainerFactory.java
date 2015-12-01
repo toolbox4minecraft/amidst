fc7af0c25dc33110af598b6b24fa8b93317305b4
Stefan Dollase <stefandollase@gmail.com>
2015-12-01 06:57:37 +0100

    collected all the layer declarations

--- src/amidst/map/LayerContainerFactory.java
+++ src/amidst/map/LayerContainerFactory.java
@@ -1,5 +1,8 @@
 package amidst.map;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import amidst.Options;
 import amidst.fragment.colorprovider.BiomeColorProvider;
 import amidst.fragment.colorprovider.SlimeColorProvider;
@@ -19,23 +22,32 @@ import amidst.minecraft.world.Resolution;
 import amidst.minecraft.world.World;
 
 public class LayerContainerFactory {
-	private final Options options;
 	private final LayerContainer layerContainer;
 
 	// @formatter:off
 	public LayerContainerFactory(Options options, World world, Map map) {
-		this.options = options;
+		List<LayerDeclaration> declarations = new ArrayList<LayerDeclaration>(10);
+		declarations.add(new LayerDeclaration(LayerType.BIOME, options.showBiomes));
+		declarations.add(new LayerDeclaration(LayerType.SLIME, options.showSlimeChunks));
+		declarations.add(new LayerDeclaration(LayerType.GRID, options.showGrid));
+		declarations.add(new LayerDeclaration(LayerType.VILLAGE, options.showVillages));
+		declarations.add(new LayerDeclaration(LayerType.OCEAN_MONUMENT, options.showOceanMonuments));
+		declarations.add(new LayerDeclaration(LayerType.STRONGHOLD, options.showStrongholds));
+		declarations.add(new LayerDeclaration(LayerType.TEMPLE, options.showTemples));
+		declarations.add(new LayerDeclaration(LayerType.SPAWN, options.showSpawn));
+		declarations.add(new LayerDeclaration(LayerType.NETHER_FORTRESS, options.showNetherFortresses));
+		declarations.add(new LayerDeclaration(LayerType.PLAYER, options.showPlayers));
 		this.layerContainer = new LayerContainer(
-				createBiomeLayer(world, map),
-				createSlimeLayer(world),
-				createGridLayer(map),
-				createVillageLayer(world, map),
-				createOceanMonumentLayer(world, map),
-				createStrongholdLayer(world, map),
-				createTempleLayer(world, map),
-				createSpawnLayer(world, map),
-				createNetherFortressLayer(world, map),
-				createPlayerLayer(world, map)
+				createBiomeLayer(declarations.get(0), world, map),
+				createSlimeLayer(declarations.get(1), world),
+				createGridLayer(declarations.get(2), map),
+				createVillageLayer(declarations.get(3), world, map),
+				createOceanMonumentLayer(declarations.get(4), world, map),
+				createStrongholdLayer(declarations.get(5), world, map),
+				createTempleLayer(declarations.get(6), world, map),
+				createSpawnLayer(declarations.get(7), world, map),
+				createNetherFortressLayer(declarations.get(8), world, map),
+				createPlayerLayer(declarations.get(9), world, map)
 		);
 	}
 
@@ -43,80 +55,70 @@ public class LayerContainerFactory {
 		return layerContainer;
 	}
 
-	private Layer createBiomeLayer(World world, Map map) {
-		LayerDeclaration declaration = new LayerDeclaration(LayerType.BIOME, options.showBiomes);
+	private Layer createBiomeLayer(LayerDeclaration declaration, World world, Map map) {
 		return new Layer(declaration,
 				new BiomeDataConstructor(declaration, Resolution.QUARTER),
 				new BiomeDataLoader(declaration, Resolution.QUARTER, new BiomeColorProvider(map), world.getBiomeDataOracle()),
 				new ImageDrawer(declaration, Resolution.QUARTER));
 	}
 
-	private Layer createSlimeLayer(World world) {
-		LayerDeclaration declaration = new LayerDeclaration(LayerType.SLIME, options.showSlimeChunks);
+	private Layer createSlimeLayer(LayerDeclaration declaration, World world) {
 		return new Layer(declaration,
 				new ImageConstructor(declaration, Resolution.CHUNK),
 				new ImageLoader(declaration, Resolution.CHUNK, new SlimeColorProvider(world)),
 				new ImageDrawer(declaration, Resolution.CHUNK));
 	}
 
-	private Layer createGridLayer(Map map) {
-		LayerDeclaration declaration = new LayerDeclaration(LayerType.GRID, options.showGrid);
+	private Layer createGridLayer(LayerDeclaration declaration, Map map) {
 		return new Layer(declaration,
 				new DummyConstructor(),
 				new DummyLoader(),
 				new GridDrawer(map));
 	}
 
-	private Layer createVillageLayer(World world, Map map) {
-		LayerDeclaration declaration = new LayerDeclaration(LayerType.VILLAGE, options.showVillages);
+	private Layer createVillageLayer(LayerDeclaration declaration, World world, Map map) {
 		return new Layer(declaration,
 				new DummyConstructor(),
 				new WorldObjectLoader(declaration, world.getVillageProducer()),
 				new WorldObjectDrawer(declaration, map));
 	}
 
-	private Layer createOceanMonumentLayer(World world, Map map) {
-		LayerDeclaration declaration = new LayerDeclaration(LayerType.OCEAN_MONUMENT, options.showOceanMonuments);
+	private Layer createOceanMonumentLayer(LayerDeclaration declaration, World world, Map map) {
 		return new Layer(declaration,
 				new DummyConstructor(),
 				new WorldObjectLoader(declaration, world.getOceanMonumentProducer()),
 				new WorldObjectDrawer(declaration, map));
 	}
 
-	private Layer createStrongholdLayer(World world, Map map) {
-		LayerDeclaration declaration = new LayerDeclaration(LayerType.STRONGHOLD, options.showStrongholds);
+	private Layer createStrongholdLayer(LayerDeclaration declaration, World world, Map map) {
 		return new Layer(declaration,
 				new DummyConstructor(),
 				new WorldObjectLoader(declaration, world.getStrongholdProducer()),
 				new WorldObjectDrawer(declaration, map));
 	}
 
-	private Layer createTempleLayer(World world, Map map) {
-		LayerDeclaration declaration = new LayerDeclaration(LayerType.TEMPLE, options.showTemples);
+	private Layer createTempleLayer(LayerDeclaration declaration, World world, Map map) {
 		return new Layer(declaration,
 				new DummyConstructor(),
 				new WorldObjectLoader(declaration, world.getTempleProducer()),
 				new WorldObjectDrawer(declaration, map));
 	}
 
-	private Layer createSpawnLayer(World world, Map map) {
-		LayerDeclaration declaration = new LayerDeclaration(LayerType.SPAWN, options.showSpawn);
+	private Layer createSpawnLayer(LayerDeclaration declaration, World world, Map map) {
 		return new Layer(declaration,
 				new DummyConstructor(),
 				new WorldObjectLoader(declaration, world.getSpawnProducer()),
 				new WorldObjectDrawer(declaration, map));
 	}
 
-	private Layer createNetherFortressLayer(World world, Map map) {
-		LayerDeclaration declaration = new LayerDeclaration(LayerType.NETHER_FORTRESS, options.showNetherFortresses);
+	private Layer createNetherFortressLayer(LayerDeclaration declaration, World world, Map map) {
 		return new Layer(declaration,
 				new DummyConstructor(),
 				new WorldObjectLoader(declaration, world.getNetherFortressProducer()),
 				new WorldObjectDrawer(declaration, map));
 	}
 
-	private Layer createPlayerLayer(World world, Map map) {
-		LayerDeclaration declaration = new LayerDeclaration(LayerType.PLAYER, options.showPlayers);
+	private Layer createPlayerLayer(LayerDeclaration declaration, World world, Map map) {
 		return new Layer(declaration,
 				new DummyConstructor(),
 				new WorldObjectLoader(declaration, world.getPlayerProducer()),
