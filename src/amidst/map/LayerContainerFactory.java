2d3e584fdb2b11c2cd7255e606ca6d1ef8a6b90c
Stefan Dollase <stefandollase@gmail.com>
2015-12-01 05:06:40 +0100

    removed all the layer derived classes, since they were only constructors for the class Layer

--- src/amidst/map/LayerContainerFactory.java
+++ src/amidst/map/LayerContainerFactory.java
@@ -3,14 +3,24 @@ package amidst.map;
 import amidst.Options;
 import amidst.fragment.colorprovider.BiomeColorProvider;
 import amidst.fragment.colorprovider.SlimeColorProvider;
-import amidst.map.layer.BiomeLayer;
-import amidst.map.layer.GridLayer;
-import amidst.map.layer.ImageLayer;
+import amidst.fragment.constructor.BiomeDataConstructor;
+import amidst.fragment.constructor.DummyConstructor;
+import amidst.fragment.constructor.ImageConstructor;
+import amidst.fragment.drawer.GridDrawer;
+import amidst.fragment.drawer.ImageDrawer;
+import amidst.fragment.drawer.WorldObjectDrawer;
+import amidst.fragment.loader.BiomeDataLoader;
+import amidst.fragment.loader.DummyLoader;
+import amidst.fragment.loader.ImageLoader;
+import amidst.fragment.loader.WorldObjectLoader;
 import amidst.map.layer.Layer;
 import amidst.map.layer.LayerType;
-import amidst.map.layer.WorldObjectLayer;
+import amidst.minecraft.world.BiomeDataOracle;
 import amidst.minecraft.world.Resolution;
 import amidst.minecraft.world.World;
+import amidst.minecraft.world.object.WorldObjectProducer;
+import amidst.preferences.BooleanPrefModel;
+import amidst.preferences.PrefModel;
 
 public class LayerContainerFactory {
 	private Options options;
@@ -37,54 +47,87 @@ public class LayerContainerFactory {
 	}
 
 	private Layer createBiomeLayer(World world, Map map) {
-		return new BiomeLayer(LayerType.BIOME, options.showBiomes,
+		return doCreateBiomeLayer(LayerType.BIOME, options.showBiomes,
 				new BiomeColorProvider(map), Resolution.QUARTER,
 				world.getBiomeDataOracle());
 	}
 
+	private Layer doCreateBiomeLayer(LayerType layerType,
+			PrefModel<Boolean> isVisiblePreference,
+			BiomeColorProvider colorProvider, Resolution resolution,
+			BiomeDataOracle biomeDataOracle) {
+		return new Layer(layerType, isVisiblePreference, new ImageDrawer(
+				resolution, layerType), new BiomeDataConstructor(layerType,
+				resolution), new BiomeDataLoader(layerType, colorProvider,
+				resolution, biomeDataOracle));
+	}
+
 	private Layer createSlimeLayer(World world) {
-		return new ImageLayer(LayerType.SLIME, options.showSlimeChunks,
+		return doCreateImageLayer(LayerType.SLIME, options.showSlimeChunks,
 				new SlimeColorProvider(world), Resolution.CHUNK);
 	}
 
+	private Layer doCreateImageLayer(LayerType layerType,
+			BooleanPrefModel isVisiblePreference,
+			SlimeColorProvider colorProvider, Resolution resolution) {
+		return new Layer(layerType, isVisiblePreference, new ImageDrawer(
+				resolution, layerType), new ImageConstructor(layerType,
+				resolution), new ImageLoader(layerType, colorProvider,
+				resolution));
+	}
+
 	private Layer createGridLayer(Map map) {
-		return new GridLayer(LayerType.GRID, options.showGrid, map);
+		return doCreateGridLayer(LayerType.GRID, options.showGrid, map);
+	}
+
+	private Layer doCreateGridLayer(LayerType layerType,
+			BooleanPrefModel isVisiblePreference, Map map) {
+		return new Layer(layerType, isVisiblePreference, new GridDrawer(map),
+				new DummyConstructor(), new DummyLoader());
 	}
 
 	private Layer createVillageLayer(World world, Map map) {
-		return new WorldObjectLayer(LayerType.VILLAGE, options.showVillages,
-				map, world.getVillageProducer());
+		return doCreateWorldObjectLayer(LayerType.VILLAGE,
+				options.showVillages, map, world.getVillageProducer());
 	}
 
 	private Layer createOceanMonumentLayer(World world, Map map) {
-		return new WorldObjectLayer(LayerType.OCEAN_MONUMENT,
+		return doCreateWorldObjectLayer(LayerType.OCEAN_MONUMENT,
 				options.showOceanMonuments, map,
 				world.getOceanMonumentProducer());
 	}
 
 	private Layer createStrongholdLayer(World world, Map map) {
-		return new WorldObjectLayer(LayerType.STRONGHOLD,
+		return doCreateWorldObjectLayer(LayerType.STRONGHOLD,
 				options.showStrongholds, map, world.getStrongholdProducer());
 	}
 
 	private Layer createTempleLayer(World world, Map map) {
-		return new WorldObjectLayer(LayerType.TEMPLE, options.showTemples, map,
-				world.getTempleProducer());
+		return doCreateWorldObjectLayer(LayerType.TEMPLE, options.showTemples,
+				map, world.getTempleProducer());
 	}
 
 	private Layer createSpawnLayer(World world, Map map) {
-		return new WorldObjectLayer(LayerType.SPAWN, options.showSpawn, map,
-				world.getSpawnProducer());
+		return doCreateWorldObjectLayer(LayerType.SPAWN, options.showSpawn,
+				map, world.getSpawnProducer());
 	}
 
 	private Layer createNetherFortressLayer(World world, Map map) {
-		return new WorldObjectLayer(LayerType.NETHER_FORTRESS,
+		return doCreateWorldObjectLayer(LayerType.NETHER_FORTRESS,
 				options.showNetherFortresses, map,
 				world.getNetherFortressProducer());
 	}
 
 	private Layer createPlayerLayer(World world, Map map) {
-		return new WorldObjectLayer(LayerType.PLAYER, options.showPlayers, map,
-				world.getPlayerProducer());
+		return doCreateWorldObjectLayer(LayerType.PLAYER, options.showPlayers,
+				map, world.getPlayerProducer());
+	}
+
+	private Layer doCreateWorldObjectLayer(LayerType layerType,
+			BooleanPrefModel isVisiblePreference, Map map,
+			WorldObjectProducer producer) {
+		return new Layer(layerType, isVisiblePreference, new WorldObjectDrawer(
+				map, layerType), new DummyConstructor(), new WorldObjectLoader(
+				layerType, producer));
 	}
 }
