d0e24bf95c4fc939f0fe7c6251269e1bdbba6a87
Stefan Dollase <stefandollase@gmail.com>
2015-12-05 23:59:01 +0100

    added the instance variable VersionList.latest

---
+++ src/amidst/mojangapi/versionlist/Latest.java
@@ -0,0 +1,18 @@
+package amidst.mojangapi.versionlist;
+
+public class Latest {
+	private String snapshot;
+	private String release;
+
+	public Latest() {
+		// no-argument constructor for gson
+	}
+
+	public String getSnapshot() {
+		return snapshot;
+	}
+
+	public String getRelease() {
+		return release;
+	}
+}
