a09b70cd8285f72c518fb89dc4b5f157bc159897
Stefan Dollase <stefandollase@gmail.com>
2015-12-05 20:15:26 +0100

    copied the package mojangapi from the branch refactoring to this branch

---
+++ src/amidst/mojangapi/profile/Latest.java
@@ -0,0 +1,18 @@
+package amidst.mojangapi.profile;
+
+public class Latest {
+	private String snapshot;
+	private String release;
+
+	public Latest() {
+		// no-argument constructor for gson
+	}
+
+	public String getSnapshot() {
+		return snapshot;
+	}
+
+	public String getRelease() {
+		return release;
+	}
+}
---
+++ src/amidst/mojangapi/profile/Versions.java
@@ -0,0 +1,35 @@
+package amidst.mojangapi.profile;
+
+import java.io.IOException;
+import java.util.List;
+
+import amidst.utilities.URIUtils;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonIOException;
+import com.google.gson.JsonSyntaxException;
+
+public class Versions {
+	private static final Gson GSON = new Gson();
+	private static final String VERSIONS_URL = "https://s3.amazonaws.com/Minecraft.Download/versions/versions.json";
+
+	public static Versions retrieve() throws JsonSyntaxException,
+			JsonIOException, IOException {
+		return GSON.fromJson(URIUtils.newReader(VERSIONS_URL), Versions.class);
+	}
+
+	private Latest latest;
+	private List<Version> versions;
+
+	public Versions() {
+		// no-argument constructor for gson
+	}
+
+	public Latest getLatest() {
+		return latest;
+	}
+
+	public List<Version> getVersions() {
+		return versions;
+	}
+}
