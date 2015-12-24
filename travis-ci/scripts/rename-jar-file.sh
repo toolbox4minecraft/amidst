73d96e626466588e4754e5baa07c931f87d06915
Stefan Dollase <stefandollase@gmail.com>
2015-12-24 03:18:08 +0100

    switched from the maven-assembly-plugin to the maven-shade-plugin
    * this seems to work better when building fat jar files
    * it replaced the default output jar file (without dependencies) by the fat jar file (including all dependencies)
    * removed the script that replaced the default jar file by the fat jar file
    * enabled the generation of the mac wrapper, since it should work
    * the mac wrapper will contain all dependencies twice

--- travis-ci/scripts/rename-jar-file.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-filename="`source travis-ci/scripts/get-amidst-filename.sh`"
-rm "target/${filename}.jar"
-mv "target/${filename}-jar-with-dependencies.jar" "target/${filename}.jar"
-
--- travis-ci/scripts/zip-mac-app.sh
+++ travis-ci/scripts/zip-mac-app.sh
@@ -2,6 +2,5 @@
 
 filename="`source travis-ci/scripts/get-amidst-filename.sh`"
 cd "travis-ci/wrapper-for-mac/target/${filename}/"
-rm "${filename}.zip"
 zip -r "${filename}.zip" "AMIDST.app/"
 mv "${filename}.zip" "../../../../target/"
