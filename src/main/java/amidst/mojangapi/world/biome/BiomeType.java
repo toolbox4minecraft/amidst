2416ea365ec2c98034e7a0ea167cf0cc84fc8471
Stefan Dollase <stefandollase@gmail.com>
2015-12-25 16:09:45 +0100

    renamed the biome type constants to upper case

--- src/main/java/amidst/mojangapi/world/biome/BiomeType.java
+++ src/main/java/amidst/mojangapi/world/biome/BiomeType.java
@@ -6,19 +6,19 @@ import amidst.documentation.Immutable;
 @Immutable
 public class BiomeType {
 	// @formatter:off
-	public static final BiomeType typeA = new BiomeType( 0.1F,   0.2F);
-	public static final BiomeType typeB = new BiomeType(-0.5F,   0.0F);
-	public static final BiomeType typeC = new BiomeType(-1.0F,   0.1F);
-	public static final BiomeType typeD = new BiomeType(-1.8F,   0.1F);
-	public static final BiomeType typeE = new BiomeType( 0.125F, 0.05F);
-	public static final BiomeType typeF = new BiomeType( 0.2F,   0.2F);
-	public static final BiomeType typeG = new BiomeType( 0.45F,  0.3F);
-	public static final BiomeType typeH = new BiomeType( 1.5F,   0.025F);
-	public static final BiomeType typeI = new BiomeType( 1.0F,   0.5F);
-	public static final BiomeType typeJ = new BiomeType( 0.0F,   0.025F);
-	public static final BiomeType typeK = new BiomeType( 0.1F,   0.8F);
-	public static final BiomeType typeL = new BiomeType( 0.2F,   0.3F);
-	public static final BiomeType typeM = new BiomeType(-0.2F,   0.1F);
+	public static final BiomeType TYPE_A = new BiomeType( 0.1F,   0.2F);
+	public static final BiomeType TYPE_B = new BiomeType(-0.5F,   0.0F);
+	public static final BiomeType TYPE_C = new BiomeType(-1.0F,   0.1F);
+	public static final BiomeType TYPE_D = new BiomeType(-1.8F,   0.1F);
+	public static final BiomeType TYPE_E = new BiomeType( 0.125F, 0.05F);
+	public static final BiomeType TYPE_F = new BiomeType( 0.2F,   0.2F);
+	public static final BiomeType TYPE_G = new BiomeType( 0.45F,  0.3F);
+	public static final BiomeType TYPE_H = new BiomeType( 1.5F,   0.025F);
+	public static final BiomeType TYPE_I = new BiomeType( 1.0F,   0.5F);
+	public static final BiomeType TYPE_J = new BiomeType( 0.0F,   0.025F);
+	public static final BiomeType TYPE_K = new BiomeType( 0.1F,   0.8F);
+	public static final BiomeType TYPE_L = new BiomeType( 0.2F,   0.3F);
+	public static final BiomeType TYPE_M = new BiomeType(-0.2F,   0.1F);
 	// @formatter:on
 
 	private final float value1;
