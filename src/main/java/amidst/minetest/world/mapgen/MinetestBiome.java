6f62a0b27afdf5beb50fa51ff6245ada5eb6f677
Treer <treer.git+github@the-bordello.com>
2018-06-17 02:42:09 +1000

    Fix #14, Open sky in v7 floatlands should be called “Air”

--- src/main/java/amidst/minetest/world/mapgen/MinetestBiome.java
+++ src/main/java/amidst/minetest/world/mapgen/MinetestBiome.java
@@ -9,7 +9,7 @@ public class MinetestBiome extends BiomeBase {
 	// The index for out-of-bounds biomes must be (Short.MIN_VALUE + x) rather than -x, as values  
 	// like -1 would be destroyed by applying a bitplane mask. 
 	public static MinetestBiome NONE = new MinetestBiome("None", Short.MIN_VALUE + 1, BiomeColor.from(0, 0, 0), (short)0, (short)0, 0, 0);
-	public static MinetestBiome VOID = new MinetestBiome("Void", Short.MIN_VALUE + 2, BiomeColor.transparent(), (short)0, (short)0, 0, 0);
+	public static MinetestBiome VOID = new MinetestBiome("Air", Short.MIN_VALUE + 2, BiomeColor.transparent(), (short)0, (short)0, 0, 0);
 
 	protected MinetestBiome(String name, BiomeColor defaultColor, short y_min, short y_max, float heat_point, float humidity_point) {
 		this(name, -1, defaultColor, y_min, y_max, heat_point, humidity_point);
