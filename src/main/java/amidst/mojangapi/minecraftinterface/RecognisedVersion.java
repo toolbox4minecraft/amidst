19387e6aa75687f32319f54b274dd57d229c107f
burgerguy <ryan.gehling@yahoo.com>
2020-02-08 20:27:57 -0500

    update link

--- src/main/java/amidst/mojangapi/minecraftinterface/RecognisedVersion.java
+++ src/main/java/amidst/mojangapi/minecraftinterface/RecognisedVersion.java
@@ -19,7 +19,7 @@ public enum RecognisedVersion {
 	// Make sure UNKNOWN is the first entry, so it is always considered newer than all other versions, since an unknown version is most likely a new snapshot.
 	// The 128 stronghold algorithm changes in version 16w06a. However, we cannot uniquely identify this version.
 	// 1.8.4, 1.8.5, 1.8.6, 1.8.7, and 1.8.8 all have the same typeDump version ID. They are all security issue fixes.
-	// 1.8.3 and 1.8.2 have the same typeDump version ID - probably because 1.8.2 -> 1.8.3 was a fix for a server-side bug (https://mojang.com/2015/02/minecraft-1-8-2-is-now-available/)
+	// 1.8.3 and 1.8.2 have the same typeDump version ID - probably because 1.8.2 -> 1.8.3 was a fix for a server-side bug (https://minecraft.gamepedia.com/Java_Edition_1.8.3/)
 	// TODO: Remove these versions before V1_0?
 	// TODO: stronghold reset on V1_9pre4?
 	UNKNOWN,
