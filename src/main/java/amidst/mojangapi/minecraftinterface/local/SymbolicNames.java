72e703713d24a3fff18a4304cdd1fa89ba95a4b1
burgerguy <ryan.gehling@yahoo.com>
2020-02-22 16:25:53 -0500

    tiny whitespace change

--- src/main/java/amidst/mojangapi/minecraftinterface/local/SymbolicNames.java
+++ src/main/java/amidst/mojangapi/minecraftinterface/local/SymbolicNames.java
@@ -8,7 +8,7 @@ public enum SymbolicNames {
 
 	public static final String CLASS_REGISTRY = "Registry";
 	public static final String FIELD_REGISTRY_BIOME = "biomeRegistry";
-	
+
 	public static final String CLASS_MAPPED_REGISTRY = "MappedRegistry";
 	public static final String METHOD_MAPPED_REGISTRY_GET_ID = "getId";
 	public static final String METHOD_MAPPED_REGISTRY_BY_ID = "byId";
@@ -49,7 +49,7 @@ public enum SymbolicNames {
 
 	// BiomeZoomer full resolution converter
 	public static final String CLASS_NOISE_BIOME_SOURCE = "NoiseBiomeSource";
-	
+
 	public static final String CLASS_FUZZY_OFFSET_CONSTANT_COLUMN_BIOME_ZOOMER = "FuzzyOffsetConstantColumnBiomeZoomer";
 	public static final String METHOD_FUZZY_OFFSET_CONSTANT_COLUMN_BIOME_ZOOMER_GET_BIOME = "getBiome";
 }
