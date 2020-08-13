4b176220e29ea8e12e80f1e24e36aa8647ab33da
burgerguy <ryan.gehling@yahoo.com>
2020-08-13 17:29:45 -0400

    Move the component listener out of PerMainWindowInjector

--- src/main/java/amidst/gui/main/PerMainWindowInjector.java
+++ src/main/java/amidst/gui/main/PerMainWindowInjector.java
@@ -1,16 +1,6 @@
 package amidst.gui.main;
 
 import java.awt.Container;
-import java.awt.GraphicsConfiguration;
-import java.awt.GraphicsDevice;
-import java.awt.GraphicsEnvironment;
-import java.awt.Window;
-import java.awt.event.ComponentEvent;
-import java.awt.event.ComponentListener;
-import java.lang.invoke.MethodHandle;
-import java.lang.invoke.MethodHandles;
-import java.lang.invoke.MethodType;
-import java.lang.reflect.Method;
 import java.util.concurrent.atomic.AtomicReference;
 
 import javax.swing.JFrame;
@@ -30,7 +20,6 @@ import amidst.gui.main.menu.AmidstMenuBuilder;
 import amidst.gui.main.viewer.ViewerFacade;
 import amidst.gui.seedsearcher.SeedSearcher;
 import amidst.gui.seedsearcher.SeedSearcherWindow;
-import amidst.logging.AmidstLogger;
 import amidst.mojangapi.RunningLauncherProfile;
 import amidst.mojangapi.file.MinecraftInstallation;
 import amidst.mojangapi.world.World;
@@ -81,53 +70,7 @@ public class PerMainWindowInjector {
 		this.viewerFacadeFactory = viewerFacadeFactory;
 		this.versionString = createVersionString(metadata, runningLauncherProfile);
 		this.frame = new JFrame();
-		// patch for multiple monitors
-		frame.addComponentListener(new ComponentListener() {
-			private final MethodHandle setGraphicsConfiguration;
-			private boolean errorPrinted = false;
-			
-			{
-				MethodHandle mh1 = null;
-				
-				try {
-					Method m1 = Window.class.getDeclaredMethod("setGraphicsConfiguration", GraphicsConfiguration.class);
-					m1.setAccessible(true);
-					mh1 = MethodHandles.lookup().unreflect(m1);
-					mh1 = mh1.asType(MethodType.methodType(void.class, JFrame.class, GraphicsConfiguration.class)); // change to allow invokeExact
-				} catch (NoSuchMethodException | IllegalAccessException e) {
-					AmidstLogger.error(e, "Unable to get setGraphicsConfiguration method");
-				}
-				
-				this.setGraphicsConfiguration = mh1;
-			}
-
-			public void componentResized(ComponentEvent e) { updateGC(); }
-			public void componentMoved(ComponentEvent e) { updateGC(); }
-			public void componentShown(ComponentEvent e) { updateGC(); }
-			public void componentHidden(ComponentEvent e) { updateGC(); }
-			
-			private void updateGC() {
-				try {
-					for (GraphicsDevice gd : GraphicsEnvironment.getLocalGraphicsEnvironment().getScreenDevices()){
-						GraphicsConfiguration defaultConfig = gd.getDefaultConfiguration();
-						if (!frame.getGraphicsConfiguration().equals(defaultConfig)) {
-						    if (frame.getLocation().getX() >= defaultConfig.getBounds().getMinX() &&
-						        frame.getLocation().getX() < defaultConfig.getBounds().getMaxX() &&
-						        frame.getLocation().getY() >= defaultConfig.getBounds().getMinY() &&
-						        frame.getLocation().getY() < defaultConfig.getBounds().getMaxY()) {
-						    	setGraphicsConfiguration.invokeExact(frame, defaultConfig);
-						    }
-						}
-					}
-				} catch (Throwable t) {
-					if(!errorPrinted) {
-						AmidstLogger.error(t, "Unable to set GraphicsConfiguration");
-						errorPrinted = true;
-					}
-				}
-			}
-			
-		});
+		frame.addComponentListener(new MultiMonitorFixer(frame));
 		this.contentPane = frame.getContentPane();
 		this.viewerFacadeReference = new AtomicReference<>();
 		this.dialogs = new MainWindowDialogs(settings, runningLauncherProfile, frame);
