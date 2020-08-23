e249a2c4fdcb6cc0ebd5b4f4955a6a4ffd8a6fb1
burgerguy <ryan.gehling@yahoo.com>
2020-08-23 19:01:29 -0400

    fix tests

--- src/main/java/amidst/mojangapi/world/icon/producer/NoopProducer.java
+++ src/main/java/amidst/mojangapi/world/icon/producer/NoopProducer.java
@@ -5,9 +5,9 @@ import java.util.function.Consumer;
 import amidst.mojangapi.world.coordinates.CoordinatesInWorld;
 import amidst.mojangapi.world.icon.WorldIcon;
 
-public class NoopProducer extends WorldIconProducer<Void> {
+public class NoopProducer extends WorldIconProducer<Object> {
 
 	@Override
-	public void produce(CoordinatesInWorld corner, Consumer<WorldIcon> consumer, Void additionalData) {}
+	public void produce(CoordinatesInWorld corner, Consumer<WorldIcon> consumer, Object additionalData) {}
 	
 }
