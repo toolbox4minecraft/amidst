15b1f61ed43f7b632a82a9327a22425eeacc14db
burgerguy <ryan.gehling@yahoo.com>
2020-09-21 12:37:04 -0400

    remove accidental inverse

--- src/main/java/amidst/fragment/FragmentManager.java
+++ src/main/java/amidst/fragment/FragmentManager.java
@@ -97,7 +97,7 @@ public class FragmentManager {
 	 */
 	@CalledOnlyBy(AmidstThread.EDT)
 	public void retireFragment(Fragment fragment) {
-		if (!fragment.isLoaded() && !fragment.recycle()) { // try to recycle if it's not loaded
+		if (!fragment.isLoaded() && fragment.recycle()) { // try to recycle if it's not loaded
 			// Send it back to the available cache
 			// We also don't want this to load while it's in the availableCache so we remove it from the loadingQueue
 			while (loadingQueue.remove(fragment));
