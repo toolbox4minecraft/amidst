f3b7e01f7cf9732e4c3ad0b3687a68513b1c4d4a
burgerguy <ryan.gehling@yahoo.com>
2020-09-22 11:13:34 -0400

    Update Fragment.java

--- src/main/java/amidst/fragment/Fragment.java
+++ src/main/java/amidst/fragment/Fragment.java
@@ -23,43 +23,18 @@ import amidst.mojangapi.world.oracle.EndIsland;
  * </br>
  * The life-cycle of a Fragment is quite complex to prevent the garbage
  * collection from running too often. The cycle consists of four states:
- * uninitialized, initialized, loading, and loaded. These all govern what can be
- * done to the fragment and any given time. A fragment starts off as
- * uninitialized on creation. Soon after its creation, the EDT sets some of its
- * variables and changes the state to initialized. From there, the fragment
- * is put into the fragment graph to be displayed, and in the loading queue
- * to be loaded. The fragment loader and worker threads are the only threads
- * that can modify its state beyond this point. The loading queue is polled by
- * the fragment worker threads and the fragment retrieved is set to LOADING to be
- * loaded. Once it's finished loading, it gets set to LOADED.</br>
- * </br>
- * When a fragment ends up going off screen, a check is done to determine how to
- * use it. If the fragment is loading or loaded, it gets sent to the off-screen
- * cache, where it will be able to exist for a set amount of time without being
- * refreshed before it gets cleaned out of the queue and garbage collected. If
- * the fragment isn't being loaded or loading, it gets recycled. When a fragment
- * is recycled, its state gets reset to initialized and it gets sent to the
- * available cache to be re-used.</br>
- * </br>
- * It is possible that a thread that uses the data in the fragment continues to
- * use them after isLoaded is set to false. However, all write operations are
- * called from either the fragment loading thread, the threads from
- * {@link FragmentManager#fragWorkers}, or the EDT during theconstruction of
- * the fragment. While the fragment is constructed it will only be accessible
- * by one thread. An exception to that rule is the instance variable alpha.
- * It is altered from the drawing thread, however this should not cause any
- * issues. </br>
- * </br>
- * Immediately after a new instance of this class is created, it is passed to
- * all FragmentConstructors. At that point in time, no other thread can access
- * the fragment, so the whole construction process is single-threaded. After the
- * fragment is constructed it will be available to use in the fragment graph. As
- * soon as it is requested, its isInitialized variable will be set to true by
- * the requesting thread. Also, it is enqueued to the loading queue. Note, that
- * the fragment is still not loaded, but used in the fragment graph and thus
+ * uninitialized, initialized, loading, and loaded. These all govern what can
+ * be done to the fragment and any given time. A fragment starts off as
+ * uninitialized on creation. Immediately after a new fragment is created, it
+ * is passed to all FragmentConstructors by the EDT. At that point in time, no
+ * other thread can access the fragment, so the whole construction process is
+ * single-threaded. After the fragment is constructed, its state gets set to
+ * initialized, and is put in both the fragment graph in the loading queue. Note,
+ * that the fragment is still not loaded, but used in the fragment graph and thus
  * used by the {@link Drawer}. Sometime after the fragment was requested, it
- * will go through the loading process, because it was enqueued to the loading
- * queue. </br>
+ * will go through the loading process because it was enqueued to the loading
+ * queue. The fragment loader and worker threads are the only threads
+ * that can modify its state beyond this point.</br>
  * </br>
  * During the loading process, the fragment loader thread will first check to
  * make sure that there are threads open in the fragment worker thread pool. If
@@ -69,31 +44,27 @@ import amidst.mojangapi.world.oracle.EndIsland;
  * is set to true. This is to make sure that only one thread is loading the
  * fragment at a time. </br>
  * </br>
- * When this is done, the isLoaded variable will be set to true. This allows the
+ * When this is done, the state will be set to loaded. This allows the
  * drawer to actually draw the fragment. The complete drawing process is
  * executed in the event dispatch thread. When the fragment is no longer visible
- * on the screen it will be removed from the fragment graph. However, since it
- * holds a data-structure that is quite heavy to allocate and garbage-collect,
- * the fragment will be recycled so it can be reused later. This recycling is
- * done by enqueuing the fragment to the recycle queue. The recycle queue is
- * processed by the fragment loading thread with a very high priority. Even
- * though the fragment loading thread only calls the method
- * {@link Fragment#tryRecycle()} and enqueues the fragment to the available
- * queue, it is important that this is done by the fragment loading queue. This
- * is because if any other thread sets the isLoaded variable to false, it might be
- * set to true by the fragment loading thread afterwards, because the fragment
- * was not yet loaded. This problem is solved by modifying the isLoaded variable
- * only in the fragment loading thread. This issue only arises, since the
- * fragment is already used in the fragment graph, before it is loaded. As soon
- * as it is used in the fragment graph it, can be recycled. This often leads to
- * a situation where a not yet loaded fragment gets recycled. The isInitialized
- * variable is altered by the thread that requests the new fragment, which is
- * different from the fragment loading thread. This is not an issue, since all
- * fragments in the available queue have both variables isInitialized and
- * isLoaded set to false. They are also not used in the fragment graph or
- * enqueued in the recycle queue. Therefore, there cannot be a race condition
- * because the isInitialized variable will only be set to false when it is
- * recycled.
+ * on the screen, a check is done to determine how to use it. If the fragment is
+ * loading or loaded, it gets sent to the off-screen cache, where it will be able
+ * to exist for a set amount of time without being refreshed before it gets
+ * recycled. If the fragment isn't loaded or loading, it also gets recycled.
+ * When a fragment is recycled, its state gets reset to initialized and it gets
+ * sent to the available cache to be re-used. If a fragment in the available queue
+ * doesn't get used for long enough, it gets cleared out and garbage
+ * collected.</br>
+ * </br>
+ * It is possible that a thread that uses the data in the fragment continues to
+ * use them after its state gets reset to uninitialized. However, all write
+ * operations are called from either the fragment loading thread, the threads from
+ * {@link FragmentManager#fragWorkers}, or the EDT during the construction of
+ * the fragment. While the fragment is constructed it will only be accessible
+ * by one thread. An exception to that rule is the instance variable alpha.
+ * It is altered from the drawing thread, however this should not cause any
+ * issues. </br>
+ * </br>
  */
 @ThreadSafe
 public class Fragment {
