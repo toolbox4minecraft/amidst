dca9de3a4eb649504bea401802c732bd26975d1d
burgerguy <ryan.gehling@yahoo.com>
2020-11-02 11:54:20 -0500

    remove unneeded import

--- src/main/java/amidst/mojangapi/file/service/ClassLoaderService.java
+++ src/main/java/amidst/mojangapi/file/service/ClassLoaderService.java
@@ -10,7 +10,6 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
-import java.util.Optional;
 import java.util.regex.Pattern;
 import java.util.stream.Stream;
 
