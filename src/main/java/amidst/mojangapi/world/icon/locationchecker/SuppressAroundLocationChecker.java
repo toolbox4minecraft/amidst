a04310c4420249891075fc73e46f2ee67c59d359
sarthakpatel <sr555161@dal.ca>
2023-04-06 18:30:02 -0300

    Extract Method Refactoring Implemented.

--- src/main/java/amidst/mojangapi/world/icon/locationchecker/SuppressAroundLocationChecker.java
+++ src/main/java/amidst/mojangapi/world/icon/locationchecker/SuppressAroundLocationChecker.java
@@ -26,50 +26,50 @@ public class SuppressAroundLocationChecker implements LocationChecker {
 		if (distance < 0) {
 			return true;
 		}
-		
+
 		CoordinatesInWorld thisStructPos = new CoordinatesInWorld(chunkX, chunkZ);
-		
+
 		int nwChunkX = chunkX - distance;
 		int nwChunkZ = chunkZ - distance;
 		int seChunkX = chunkX + distance;
 		int seChunkZ = chunkZ + distance;
-		
+
 		CoordinatesInWorld otherStructPos = getLocation(nwChunkX, nwChunkZ);
-		
 		if (otherStructPos != null && otherStructPos.getDistanceChebyshev(thisStructPos) <= distance) {
 			return false;
 		}
-		
+
 		int nwRegionX = regionalProducer.getRegionCoord(nwChunkX);
 		int nwRegionZ = regionalProducer.getRegionCoord(nwChunkZ);
 		int seRegionX = regionalProducer.getRegionCoord(seChunkX);
 		int seRegionZ = regionalProducer.getRegionCoord(seChunkZ);
-		
+
 		//The area is contained within one region.
 		if (nwRegionX == seRegionX && nwRegionZ == seRegionZ) {
 			return true;
 		}
-		
+
 		//The area intersects 4 regions.
 		if (nwRegionX != seRegionX && nwRegionZ != seRegionZ) {
-			
-			otherStructPos = getLocation(seChunkX, seChunkZ);
-			if (otherStructPos != null && otherStructPos.getDistanceChebyshev(thisStructPos) <= distance) {
+			if (intersectsRegion(nwChunkX, nwChunkZ, thisStructPos)) {
 				return false;
 			}
-			
-			otherStructPos = getLocation(nwChunkX, seChunkZ);
-			if (otherStructPos != null && otherStructPos.getDistanceChebyshev(thisStructPos) <= distance) {
+			if (intersectsRegion(seChunkX, seChunkZ, thisStructPos)) {
 				return false;
 			}
-			
-			otherStructPos = getLocation(seChunkX, nwChunkZ);
-			return !(otherStructPos != null && otherStructPos.getDistanceChebyshev(thisStructPos) <= distance);
+			if (intersectsRegion(nwChunkX, seChunkZ, thisStructPos)) {
+				return false;
+			}
+			return !intersectsRegion(seChunkX, nwChunkZ, thisStructPos);
 		}
-		
+
 		//The area intersects 2 regions.
-		otherStructPos = getLocation(seChunkX, seChunkZ);
-		return !(otherStructPos != null && otherStructPos.getDistanceChebyshev(thisStructPos) <= distance);
+		return !intersectsRegion(seChunkX, seChunkZ, thisStructPos);
+	}
+
+	private boolean intersectsRegion(int chunkX, int chunkZ, CoordinatesInWorld thisStructPos) {
+		CoordinatesInWorld otherStructPos = getLocation(chunkX, chunkZ);
+		return otherStructPos != null && otherStructPos.getDistanceChebyshev(thisStructPos) <= distance;
 	}
 	
 	private CoordinatesInWorld getLocation(int chunkX, int chunkZ) {
