b1fe251199c074054d064c25232e593932a0b677
SWinxy <swinxythecat@gmail.com>
2025-05-18 16:48:38 -0400

    Simplify Amidst.java
    The initialization logic is contained in the main method, rather than split into sub-methods. Removed code relating to graphics acceleration, a feature that is enabled by default on Swing; Swing defaults to the best pipeline on the system.

--- src/main/java/amidst/Amidst.java
+++ src/main/java/amidst/Amidst.java
@@ -1,16 +1,5 @@
 package amidst;
 
-import java.nio.file.Path;
-import java.sql.Timestamp;
-import java.util.Date;
-import java.util.prefs.Preferences;
-
-import javax.swing.SwingUtilities;
-
-import org.kohsuke.args4j.CmdLineException;
-import org.kohsuke.args4j.CmdLineParser;
-import org.kohsuke.args4j.ParserProperties;
-
 import amidst.documentation.AmidstThread;
 import amidst.documentation.CalledByAny;
 import amidst.documentation.CalledOnlyBy;
@@ -20,40 +9,92 @@ import amidst.logging.AmidstLogger;
 import amidst.logging.AmidstMessageBox;
 import amidst.logging.FileLogger;
 import amidst.mojangapi.file.DotMinecraftDirectoryNotFoundException;
-import amidst.util.OperatingSystemDetector;
+import org.kohsuke.args4j.CmdLineException;
+import org.kohsuke.args4j.CmdLineParser;
+import org.kohsuke.args4j.ParserProperties;
 
+import java.awt.EventQueue;
+import java.sql.Timestamp;
+import java.util.Date;
+import java.util.prefs.Preferences;
+
+/**
+ * The entry point class to the Amidst application.
+ */
 @NotThreadSafe
 public class Amidst {
+
+	/**
+	 * The entry point to the Amidst application.
+	 *
+	 * @param args command line arguments (see the wiki)
+	 */
 	@CalledOnlyBy(AmidstThread.STARTUP)
-	public static void main(String args[]) {
-		initUncaughtExceptionHandler();
-		parseCommandLineArgumentsAndRun(args);
-	}
+	public static void main(String[] args) {
+		Thread.setDefaultUncaughtExceptionHandler((thread, e) -> handleCrash(e, thread));
 
-	private static void initUncaughtExceptionHandler() {
-		Thread.setDefaultUncaughtExceptionHandler(new Thread.UncaughtExceptionHandler() {
-			@Override
-			public void uncaughtException(Thread thread, Throwable e) {
-				handleCrash(e, thread);
-			}
-		});
-	}
-
-	private static void parseCommandLineArgumentsAndRun(String[] args) {
+		// Parse CLI arguments
 		CommandLineParameters parameters = new CommandLineParameters();
 		AmidstMetaData metadata = createMetadata();
 		CmdLineParser parser = new CmdLineParser(
 				parameters,
 				ParserProperties.defaults().withShowDefaults(false).withUsageWidth(120).withOptionSorter(null));
+
 		try {
 			parser.parseArgument(args);
-			run(parameters, metadata, parser);
 		} catch (CmdLineException e) {
 			System.out.println(metadata.getVersion().createLongVersionString());
 			System.err.println(e.getMessage());
 			parser.printUsage(System.out);
 			System.exit(2);
 		}
+
+		// initialize file logging
+		if (parameters.logFile != null) {
+			AmidstLogger.info("using log file: '" + parameters.logFile + "'");
+			AmidstLogger.addListener("file", new FileLogger(parameters.logFile));
+		}
+
+		String versionString = metadata.getVersion().createLongVersionString();
+
+		// Printing the help guide prints and exits
+		if (parameters.printHelp) {
+			System.out.println(versionString);
+			parser.printUsage(System.out);
+			return;
+		}
+
+		// Printing the version prints and exits
+		if (parameters.printVersion) {
+			System.out.println(versionString);
+			return;
+		}
+
+		// Log system information
+		AmidstLogger.info(versionString);
+		AmidstLogger.info("Current system time: " + new Timestamp(new Date().getTime()));
+		AmidstLogger.info(createPropertyString("os.name"));
+		AmidstLogger.info(createPropertyString("os.version"));
+		AmidstLogger.info(createPropertyString("os.arch"));
+		AmidstLogger.info(createPropertyString("java.version"));
+		AmidstLogger.info(createPropertyString("java.vendor"));
+		AmidstLogger.info(createPropertyString("sun.arch.data.model"));
+
+		// Start application
+		EventQueue.invokeLater(() -> {
+			AmidstSettings settings = new AmidstSettings(Preferences.userNodeForPackage(Amidst.class));
+			try {
+				settings.lookAndFeel.get().tryApply();
+				new PerApplicationInjector(parameters, metadata, settings).getApplication().run();
+			} catch (DotMinecraftDirectoryNotFoundException e) {
+				AmidstLogger.warn(e);
+				AmidstMessageBox.displayError(
+						"Please install Minecraft",
+						"Amidst is not able to find your '.minecraft' directory, but it requires a working Minecraft installation.");
+			} catch (Exception e) {
+				handleCrash(e, Thread.currentThread());
+			}
+		});
 	}
 
 	public static AmidstMetaData createMetadata() {
@@ -67,43 +108,6 @@ public class Amidst {
 				ResourceLoader.getImage("/amidst/icon/amidst-256x256.png"));
 	}
 
-	private static void run(CommandLineParameters parameters, AmidstMetaData metadata, CmdLineParser parser) {
-		initFileLogger(parameters.logFile);
-		String versionString = metadata.getVersion().createLongVersionString();
-		if (parameters.printHelp) {
-			System.out.println(versionString);
-			parser.printUsage(System.out);
-		} else if (parameters.printVersion) {
-			System.out.println(versionString);
-		} else {
-			AmidstLogger.info(versionString);
-			logTimeAndProperties();
-			enableGraphicsAcceleration();
-			startApplication(parameters, metadata, createSettings());
-		}
-	}
-
-	private static void initFileLogger(Path file) {
-		if (file != null) {
-			AmidstLogger.info("using log file: '" + file + "'");
-			AmidstLogger.addListener("file", new FileLogger(file));
-		}
-	}
-
-	private static void logTimeAndProperties() {
-		AmidstLogger.info("Current system time: " + getCurrentTimeStamp());
-		AmidstLogger.info(createPropertyString("os.name"));
-		AmidstLogger.info(createPropertyString("os.version"));
-		AmidstLogger.info(createPropertyString("os.arch"));
-		AmidstLogger.info(createPropertyString("java.version"));
-		AmidstLogger.info(createPropertyString("java.vendor"));
-		AmidstLogger.info(createPropertyString("sun.arch.data.model"));
-	}
-
-	private static String getCurrentTimeStamp() {
-		return new Timestamp(new Date().getTime()).toString();
-	}
-
 	private static String createPropertyString(String key) {
 		StringBuilder b = new StringBuilder();
 		b.append("System.getProperty(\"");
@@ -114,73 +118,12 @@ public class Amidst {
 		return b.toString();
 	}
 
-	private static AmidstSettings createSettings() {
-		return new AmidstSettings(Preferences.userNodeForPackage(Amidst.class));
-	}
-
 	/**
-	 * WARNING: This method MUST be invoked before applyLookAndFeel(). The
-	 * sun.java2d.* properties have no effect after applyLookAndFeel() has
-	 * been called.
+	 * On an uncaught exception, this logs it and shows a new window.
+	 *
+	 * @param e      the uncaught exception
+	 * @param thread the thread Amidst crashed on
 	 */
-	private static void enableGraphicsAcceleration() {
-		enableOpenGLIfNecessary();
-		forceGraphicsToVRAM();
-	}
-
-	/**
-	 * We only use OpenGL on OS X, because it caused lots of bugs on Windows and
-	 * performance issues on Linux. Also, this is the behavior of Amidst v3.7.
-	 *
-	 * On Windows Direct3D is better supported and the default.
-	 *
-	 * Linux has accelerated images without activating OpenGL. The reason for
-	 * this is still unknown to the developers of Amidst.
-	 *
-	 * https://github.com/toolbox4minecraft/amidst/pull/94
-	 */
-	private static void enableOpenGLIfNecessary() {
-		if (OperatingSystemDetector.isMac()) {
-			AmidstLogger.info("Enabling OpenGL.");
-			System.setProperty("sun.java2d.opengl", "True");
-		} else {
-			AmidstLogger.info("Not using OpenGL.");
-		}
-	}
-
-	private static void forceGraphicsToVRAM() {
-		System.setProperty("sun.java2d.accthreshold", "0");
-	}
-
-	private static void applyLookAndFeel(AmidstSettings settings) {
-    	settings.lookAndFeel.get().tryApply();
-	}
-
-	private static void startApplication(
-			CommandLineParameters parameters,
-			AmidstMetaData metadata,
-			AmidstSettings settings) {
-		SwingUtilities.invokeLater(() -> doStartApplication(parameters, metadata, settings));
-	}
-
-	@CalledOnlyBy(AmidstThread.EDT)
-	private static void doStartApplication(
-			CommandLineParameters parameters,
-			AmidstMetaData metadata,
-			AmidstSettings settings) {
-		try {
-			applyLookAndFeel(settings);
-			new PerApplicationInjector(parameters, metadata, settings).getApplication().run();
-		} catch (DotMinecraftDirectoryNotFoundException e) {
-			AmidstLogger.warn(e);
-			AmidstMessageBox.displayError(
-					"Please install Minecraft",
-					"Amidst is not able to find your '.minecraft' directory, but it requires a working Minecraft installation.");
-		} catch (Exception e) {
-			handleCrash(e, Thread.currentThread());
-		}
-	}
-
 	@CalledByAny
 	private static void handleCrash(Throwable e, Thread thread) {
 		String message = "Amidst has encounted an uncaught exception on the thread " + thread;
