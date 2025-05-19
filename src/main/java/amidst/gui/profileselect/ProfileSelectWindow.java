2662158fe4b2e554f5e7fe8e895ded0699b6022c
SWinxy <swinxythecat@gmail.com>
2025-05-18 22:42:38 -0400

    Simplify ProfileSelectWindow
    Everything initialized is in the, well, initializer.

--- src/main/java/amidst/gui/profileselect/ProfileSelectWindow.java
+++ src/main/java/amidst/gui/profileselect/ProfileSelectWindow.java
@@ -1,18 +1,5 @@
 package amidst.gui.profileselect;
 
-import java.awt.Font;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
-import java.io.IOException;
-import java.util.List;
-
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JScrollPane;
-import javax.swing.SwingConstants;
-import javax.swing.UIManager;
-
-import net.miginfocom.swing.MigLayout;
 import amidst.AmidstMetaData;
 import amidst.AmidstSettings;
 import amidst.Application;
@@ -25,24 +12,31 @@ import amidst.mojangapi.LauncherProfileRunner;
 import amidst.mojangapi.file.MinecraftInstallation;
 import amidst.mojangapi.file.UnresolvedLauncherProfile;
 import amidst.mojangapi.file.VersionListProvider;
-import amidst.parsing.FormatException;
 import amidst.threading.WorkerExecutor;
 import amidst.util.SwingUtils;
+import net.miginfocom.swing.MigLayout;
 
+import javax.swing.*;
+import java.awt.Font;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.util.List;
+
+/**
+ * A window that shows the profiles that are installed.
+ * <p>
+ * Some profiles may be modded or higher than the supported
+ * Amidst application.
+ */
 @NotThreadSafe
 public class ProfileSelectWindow {
+
 	private static final int HEIGHT_IN_PROFILE_COMPONENTS = 13;
 
-	private final Application application;
-	private final AmidstMetaData metadata;
-	private final WorkerExecutor workerExecutor;
-	private final VersionListProvider versionListProvider;
-	private final MinecraftInstallation minecraftInstallation;
-	private final LauncherProfileRunner launcherProfileRunner;
-	private final AmidstSettings settings;
-
+	/**
+	 * The window that contains the profile selection list.
+	 */
 	private final JFrame frame;
-	private final ProfileSelectPanel profileSelectPanel;
 
 	private volatile boolean isDisposed = false;
 
@@ -55,25 +49,32 @@ public class ProfileSelectWindow {
 			MinecraftInstallation minecraftInstallation,
 			LauncherProfileRunner launcherProfileRunner,
 			AmidstSettings settings) {
-		this.application = application;
-		this.metadata = metadata;
-		this.workerExecutor = workerExecutor;
-		this.versionListProvider = versionListProvider;
-		this.minecraftInstallation = minecraftInstallation;
-		this.launcherProfileRunner = launcherProfileRunner;
-		this.settings = settings;
-		this.profileSelectPanel = new ProfileSelectPanel(settings.lastProfile, "Scanning...");
-		this.frame = createFrame();
-		scanAndLoadProfilesLater();
-	}
 
-	@CalledOnlyBy(AmidstThread.EDT)
-	private JFrame createFrame() {
-		JFrame frame = new JFrame("Profile Selector");
+		frame = new JFrame("Profile Selector");
 		frame.setIconImages(metadata.getIcons());
 		frame.getContentPane().setLayout(new MigLayout());
-		frame.add(createTitleLabel(), "h 20!,w :400:, growx, pushx, wrap");
-		frame.add(createScrollPanel(), getScrollPaneLayoutString());
+
+		JLabel title = new JLabel("Please select a Minecraft profile:", SwingConstants.CENTER);
+		title.setFont(new Font("arial", Font.BOLD, 16));
+		frame.add(title, "h 20!,w :400:, growx, pushx, wrap");
+
+		ProfileSelectPanel profileSelectPanel = new ProfileSelectPanel(settings.lastProfile, "Scanning...");
+		JScrollPane scrollPane = new JScrollPane(profileSelectPanel.getComponent());
+		scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
+		scrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+		scrollPane.getVerticalScrollBar().setUnitIncrement(14);
+
+		// The preferred width should be at least a scrollbar-width wider than the
+		// ProfileComponent's preferredSize width of 500 (so 520?). The preferred
+		// height should allow the dialog to fit easily on a 720p display, while
+		// being nicely divisible by ProfileComponent's height of 40 (so 520 again
+		// then?).
+		int scrollBarWidth = (Integer) UIManager.get("ScrollBar.width");
+		int preferredWidth = ProfileComponent.PREFERRED_WIDTH + scrollBarWidth;
+		int preferredHeight = ProfileComponent.PREFERRED_HEIGHT * HEIGHT_IN_PROFILE_COMPONENTS;
+
+		frame.add(scrollPane, "grow, push, w :" + preferredWidth + ":, h 80:" + preferredHeight + ":");
+
 		frame.pack();
 		frame.addKeyListener(profileSelectPanel.createKeyListener());
 		frame.addWindowListener(new WindowAdapter() {
@@ -82,109 +83,48 @@ public class ProfileSelectWindow {
 				application.exitGracefully();
 			}
 		});
-		frame.setLocation(200, 200);
+		frame.setLocationRelativeTo(null);
 		frame.setVisible(true);
-		return frame;
-	}
 
-	@CalledOnlyBy(AmidstThread.EDT)
-	private JLabel createTitleLabel() {
-		JLabel result = new JLabel("Please select a Minecraft profile:", SwingConstants.CENTER);
-		result.setFont(new Font("arial", Font.BOLD, 16));
-		return result;
-	}
-
-	@CalledOnlyBy(AmidstThread.EDT)
-	private JScrollPane createScrollPanel() {
-		JScrollPane result = new JScrollPane(profileSelectPanel.getComponent());
-		result.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
-		result.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
-		result.getVerticalScrollBar().setUnitIncrement(14);
-		return result;
-	}
-
-	/**
-	 * The preferred width should be at least a scrollbar-width wider than the
-	 * ProfileComponent's preferredSize width of 500 (so 520?). The preferred
-	 * height should allow the dialog to fit easily on a 720p display, while
-	 * being nicely divisible by ProfileComponent's height of 40 (so 520 again
-	 * then?).
-	 */
-	@CalledOnlyBy(AmidstThread.EDT)
-	private String getScrollPaneLayoutString() {
-		int scrollBarWidth = (Integer) UIManager.get("ScrollBar.width");
-		int preferredWidth = ProfileComponent.PREFERRED_WIDTH + scrollBarWidth;
-		int preferredHeight = ProfileComponent.PREFERRED_HEIGHT * HEIGHT_IN_PROFILE_COMPONENTS;
-		return "grow, push, w :" + preferredWidth + ":, h 80:" + preferredHeight + ":";
-	}
-
-	@CalledOnlyBy(AmidstThread.EDT)
-	private void scanAndLoadProfilesLater() {
-		workerExecutor.run(this::scanAndLoadProfiles, this::displayProfiles, this::scanAndLoadProfilesFailed);
-	}
-
-	@CalledOnlyBy(AmidstThread.WORKER)
-	private List<UnresolvedLauncherProfile> scanAndLoadProfiles() throws FormatException, IOException {
-		AmidstLogger.info("Scanning for profiles.");
-		List<UnresolvedLauncherProfile> launcherProfiles = minecraftInstallation.readLauncherProfiles();
-		AmidstLogger.info("Successfully loaded profile list.");
-		return launcherProfiles;
-	}
-
-	@CalledOnlyBy(AmidstThread.EDT)
-	private void displayProfiles(List<UnresolvedLauncherProfile> launcherProfiles) {
-		createProfileComponentsIfNecessary(launcherProfiles);
-		restoreSelection();
-		frame.pack();
-	}
-
-	@CalledOnlyBy(AmidstThread.EDT)
-	private void createProfileComponentsIfNecessary(List<UnresolvedLauncherProfile> launcherProfiles) {
-		if (launcherProfiles.isEmpty()) {
-			AmidstLogger.warn("No profiles found in launcher_profiles.json");
-			profileSelectPanel.setEmptyMessage("No profiles found");
-		} else {
-			createProfileComponents(launcherProfiles);
-			versionListProvider.onDownloadRemoteFinished(this::resolveAllLater);
-			profileSelectPanel.resolveAllLater();
-		}
-	}
-
-	private void resolveAllLater() {
-		if (!isDisposed) {
-			profileSelectPanel.resolveAllLater();
-		}
-	}
-
-	@CalledOnlyBy(AmidstThread.EDT)
-	private void createProfileComponents(List<UnresolvedLauncherProfile> launcherProfiles) {
-		launcherProfiles.stream()
-				.map(p ->
-					new LocalProfileComponent(
-							application,
-							workerExecutor,
-							versionListProvider,
-							launcherProfileRunner,
-							p
-						)
-				).forEach(c -> profileSelectPanel.addProfile(c));
-	}
-
-	@CalledOnlyBy(AmidstThread.EDT)
-	private void restoreSelection() {
-		String profileName = settings.lastProfile.get();
-		if (profileName != null && !profileName.isEmpty()) {
-			profileSelectPanel.select(profileName);
-		} else {
-			profileSelectPanel.selectFirst();
-		}
-	}
-
-	@CalledOnlyBy(AmidstThread.EDT)
-	private void scanAndLoadProfilesFailed(Exception e) {
-		AmidstLogger.error(e, "Error reading launcher_profiles.json");
-		AmidstMessageBox.displayError("Error", e, "Error reading launcher_profiles.json");
-		profileSelectPanel.setEmptyMessage("Failed loading");
+		workerExecutor.run(() -> {
+			AmidstLogger.info("Scanning for profiles.");
+			List<UnresolvedLauncherProfile> launcherProfiles = minecraftInstallation.readLauncherProfiles();
+			AmidstLogger.info("Successfully loaded profile list.");
+			return launcherProfiles;
+		}, launcherProfiles -> {
+			if (launcherProfiles.isEmpty()) {
+				AmidstLogger.warn("No profiles found in launcher_profiles.json");
+				profileSelectPanel.setEmptyMessage("No profiles found");
+			} else {
+				launcherProfiles.stream()
+						.map(p ->
+								new LocalProfileComponent(
+										application,
+										workerExecutor,
+										versionListProvider,
+										launcherProfileRunner,
+										p
+								)
+						).forEach(profileSelectPanel::addProfile);
+				versionListProvider.onDownloadRemoteFinished(() -> {
+					if (!isDisposed) {
+						profileSelectPanel.resolveAllLater();
+					}
+				});
+				profileSelectPanel.resolveAllLater();
+			}
+			String profileName = settings.lastProfile.get();
+			if (profileName != null && !profileName.isEmpty()) {
+				profileSelectPanel.select(profileName);
+			} else {
+				profileSelectPanel.selectFirst();
+			}
+			frame.pack();
+		}, e -> {
+			AmidstLogger.error(e, "Error reading launcher_profiles.json");
+			AmidstMessageBox.displayError("Error", e, "Error reading launcher_profiles.json");
+			profileSelectPanel.setEmptyMessage("Failed loading");
+		});
 	}
 
 	@CalledOnlyBy(AmidstThread.EDT)
